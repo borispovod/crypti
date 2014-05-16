2c1d2d8d9b18655ee9f3608978707cc484d3ed96
Boris Povod <mrpovod@gmail.com>
2014-05-17 03:45:19 +0400

    new version
    Former-commit-id: f41e115b39df9f65a116f2194398efc5e9311dd4

--- forger/forgerprocessor.js
+++ forger/forgerprocessor.js
@@ -1,13 +1,45 @@
-var forgerprocessor = function () {
+var forgerprocessor = function (app) {
+    this.app = app;
+    this.accountprocessor = this.app.accountprocessor;
+    this.logger = this.app.logger;
     this.forgers = {};
+    this.lastBlocks = {};
+    this.hits = {};
+    this.timers = {};
 }
 
-forgerprocessor.prototype.getProcessor = function (id, cb) {
-    if (cb) {
-        cb(this.forgers[id]);
+forgerprocessor.prototype.getForgers = function (id) {
+    return this.forgers[id];
+}
+
+forgerprocessor.prototype.startForger = function (forger) {
+    if (this.forgers[forger.accountId]) {
+        return false;
+    }
+
+    this.forgers[forger.accountId] = forger;
+    forger.startForge();
+
+    this.timers[forger.accountId] = setInterval(function () {
+        forger.startForge();
+    }, 1000);
+
+    return true;
+}
+
+forgerprocessor.prototype.stopForger = function (accountId) {
+    if (this.forgers[accountId]) {
+        clearInterval(this.timers[accountId]);
+
+        delete this.timers[accountId];
+        delete this.forgers[accountId];
+        return true;
     } else {
-        return this.forgers[id];
+        return false;
     }
 }
 
-module.exports = forgerprocessor;
+
+module.exports.init = function (app) {
+    return new forgerprocessor(app);
+}
