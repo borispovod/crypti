0215569c00abf3f8ae72f8b609c09f90affb8097
Boris Povod <mrpovod@gmail.com>
2014-05-27 04:33:17 +0400

    api callbacks
    Former-commit-id: b31c52d3b31b95611012262085a8efdd4a2e8eb4 [formerly 0d415b491a1ee0ee3198fde447c6d42d66df4f06]
    Former-commit-id: 81b0582388f2a5e4b41725b565a5d82c1a976c9d

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express = require('express'),
     config = require('./config'),
     routes = require('./routes'),
-    db = require('./db.js'),
+    initDb = require('./db').initDb,
     async = require('async'),
     logger = require("./logger").logger,
     blockchain = require("./block").blockchain,
+    block = require("./block").block,
     accountprocessor = require("./account").accountprocessor,
     forgerprocessor = require("./forger").forgerprocessor,
     transactionprocessor = require("./transactions").transactionprocessor,
-    addressprocessor = require("./address").addressprocessor;
+    transaction = require("./transactions").transaction,
+    addressprocessor = require("./address").addressprocessor,
+    address = require("./address").address,
+    path = require("path");
 
 var app = express();
 
@@ -16,6 +20,11 @@ app.configure(function () {
     app.set("version", "0.1");
     app.set("address", config.get("address"));
     app.set('port', config.get('port'));
+
+    if (config.get("serveHttpWallet")) {
+        app.use(express.static(path.join(__dirname, "public")));
+    }
+
     app.use(app.router);
 });
 
@@ -65,7 +74,100 @@ async.series([
         logger.getInstance().info("Initializing address processor...");
         app.addressprocessor = new addressprocessor();
         cb();
-    }
+    },
+    function (cb) {
+        logger.getInstance().info("Initializing and scanning database...");
+        initDb(function (err, db) {
+            if (err) {
+                cb(err);
+            } else {
+                app.db = db;
+                app.db.readAllBlocks(function (err, blocks) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        async.forEach(blocks, function (item, c) {
+                            //version, id, timestamp, previousBlock, transactions, totalAmount, totalFee, payloadLength, payloadHash, generatorPublicKey, generationSignature, blockSignature
+                            var b = new block(item.version, null, item.previousBlock, [], item.totalAmount, item.totalFee, item.payloadLength, new Buffer(item.payloadHash, 'hex'), new Buffer(item.generatorPublicKey, 'hex'), new Buffer(item.generationSignature, 'hex'), new Buffer(item.blockSignature, 'hex'));
+                            var id = b.getId();
+
+                            if (!block.verifyBlockSignature() || !block.verifyGenerationSignature())  {
+                                return c("Can't verify block: " + id);
+                            }
+
+                            var q = app.db.sql.prepare("SELECT * FROM trs WHERE blockId = ?");
+                            q.bind(id);
+                            q.run(function (err, rows) {
+                                if (err) {
+                                    c(err);
+                                } else {
+                                    var transactions = [];
+                                    async.forEach(rows, function (t, _c) {
+                                        var tr = new transaction(t.type, t.id, t.timestamp, new Buffer(t.senderPublicKey, 'hex'), t.recepient, t.amount, t.deadline, t.fee, t.referencedTransaction, new Buffer(t.signature, 'hex'));
+
+                                        if (!tr.verify()) {
+                                            return _c("Can't verify transaction: " + tr.getId());
+                                        }
+
+                                        transactions.push(tr);
+                                        _c();
+                                    }, function (err) {
+                                        if (err) {
+                                            return c(err);
+                                        }
+                                        var addresses = [];
+
+                                        q = app.db.sql.prepare("SELECT * FROM addresses WHERE blockId = ?");
+                                        q.bind(id);
+                                        q.run(function (err, rows) {
+                                            if (err) {
+                                                c(err);
+                                            } else {
+                                                async.forEach(rows, function (a, _c) {
+                                                    var addr = new address(a.version, a.id, new Buffer(a.generatorPublicKey, 'hex'), new Buffer(a.publicKey, 'hex'), a.timestamp, new Buffer(a.signature, 'hex'), new Buffer(a.accountSignature, 'hex'));
+
+                                                    if (!addr.verify || addr.accountVerify()) {
+                                                        return _c("Can't verify address: " + addr.getId());
+                                                    }
+
+                                                    addresses.push(addr);
+                                                    _c();
+                                                }, function (err) {
+                                                    if (err) {
+                                                        return c(err);
+                                                    }
+
+                                                    var b = block.getBytes();
+
+                                                    for (var t in transactions) {
+                                                        buffer = Buffer.concat([buffer, transactions[t].getBytes()]);
+                                                    }
+
+                                                    for (var addr in addresses) {
+                                                        buffer = Buffer.concat([buffer, addresses[addr].getBytes()]);
+                                                    }
+
+                                                    var a = app.blockchain.pushBlock(buffer);
+
+                                                    if (!a) {
+                                                        c("Can't process block: " + b.getId());
+                                                    } else {
+                                                        c();
+                                                    }
+                                                });
+                                            }
+                                        });
+                                    });
+                                }
+                            });
+                        }, function (err) {
+                            cb(err);
+                        });
+                    }
+                });
+            }
+        });
+    },
 ], function (err) {
     if (err) {
         logger.getInstance().info("Crypti stopped!");
@@ -73,7 +175,10 @@ async.series([
     } else {
         app.listen(app.get('port'), app.get('address'), function () {
             logger.getInstance().info("Crypti started: " + app.get("address") + ":" + app.get("port"));
-            routes(app);
+
+            if (config.get("serveHttpApi")) {
+                routes(app);
+            }
         });
     }
 });
---
+++ db/index.js
@@ -0,0 +1,1 @@
+module.exports.initDb = require("./db.js").initDb;
