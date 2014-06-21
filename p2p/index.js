edd00674f5eb5bb93850d8baad960762b14dafd2
crypti <boris@crypti.me>
2014-06-21 04:29:52 +0400

    new version
    Former-commit-id: 09138ebf01147dbf83f2935b9730e734acea6534

--- p2p/index.js
+++ p2p/index.js
@@ -3,4 +3,5 @@ module.exports.initRoutes = function (app) {
 }
 
 module.exports.peerprocessor = require("./peerprocessor.js");
-module.exports.p2p = require("./seed.js")
+module.exports.p2p = require("./seed.js");
+module.exports.peer = require("./peer.js");
