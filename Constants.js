aa85419bcb221a1aaef4e380be9c19d3923c0c03
crypti <boris@crypti.me>
2014-07-05 01:58:00 +0400

    fix with previous block

--- Constants.js
+++ Constants.js
@@ -4,8 +4,8 @@ module.exports = {
     maxAmount : 100000000,
     maxBaseTarget : 100000000 * 1537228,
     two64 : "18446744073709551616",
-    maxPayloadLength : 255 * 128,
-    blockHeaderLength : 228,
+    maxPayloadLength : 255 * 136,
+    blockHeaderLength : 236,
     addressLength: 208,
     maxAddressLength: 208 * 128,
     maxClientConnections: 100
