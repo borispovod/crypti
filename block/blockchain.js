02d9fa0f01cdfa0265ae5b9608cafe9d9c1a598e
crypti <boris@crypti.me>
2014-07-05 04:29:40 +0400

    fix in send crypti
    Former-commit-id: e7ede7a0537e17436900b6bf550c49d86ae56ffc [formerly e7bc4fb7c0af195630d17a7df6571753a2f3ec77]
    Former-commit-id: a2ee1f4b71d97350879f36d4fdb0c41674211f1c

--- block/blockchain.js
+++ block/blockchain.js
@@ -18,7 +18,7 @@ var blockchain = function (app) {
     this.db = this.app.db;
     this.blocks = {};
     this.lastBlock = null;
-    this.fee = 0.00025;
+    this.fee = 1;
 }
 
 blockchain.prototype.getBlock = function (id) {
--- utils.js
+++ utils.js
@@ -39,15 +39,19 @@ module.exports = {
 Number.prototype.roundTo = function( digitsCount ){
     var digitsCount = typeof digitsCount !== 'undefined' ? digitsCount : 2;
     var s = String(this);
-    var e = s.indexOf('.');
-    if( e == -1 ) return this;
-    var c = s.length - e - 1;
-    if( c < digitsCount ) digitsCount = c;
-    var e1 = e + 1 + digitsCount;
-    var d = Number(s.substr(0,e) + s.substr(e+1, digitsCount));
-    if( s[e1] > 4 ) d += 1;
-    d /= Math.pow(10, digitsCount);
-    return d.valueOf();
+    if (s.indexOf('e') < 0) {
+        var e = s.indexOf('.');
+        if (e == -1) return this;
+        var c = s.length - e - 1;
+        if (c < digitsCount) digitsCount = c;
+        var e1 = e + 1 + digitsCount;
+        var d = Number(s.substr(0, e) + s.substr(e + 1, digitsCount));
+        if (s[e1] > 4) d += 1;
+        d /= Math.pow(10, digitsCount);
+        return d.valueOf();
+    } else {
+        return this.toFixed(digitsCount) / 1;
+    }
 }
 
 Math.roundTo = function( number ,digitsCount){
