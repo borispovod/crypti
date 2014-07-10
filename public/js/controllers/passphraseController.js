7671a44f38dd40d42b2293d6089a734d6fd210e7
Crypti <github@crypti.me>
2014-07-10 17:33:10 +0400

    colorful

--- public/js/controllers/passphraseController.js
+++ public/js/controllers/passphraseController.js
@@ -1,16 +1,20 @@
 webApp.controller('passphraseController', ['$scope', '$rootScope', '$http', "$state", "userService",
     function($rootScope, $scope, $http, $state, userService) {
         $scope.login = function(pass) {
-            $http.get("/api/unlock", { params : { secretPhrase : pass }})
-                .then(function (resp) {
-                    if (resp.data.success) {
-                        userService.setData(resp.data.address, resp.data.publickey, resp.data.balance, resp.data.unconfirmedBalance, resp.data.effectiveBalance);
-                        userService.setForging(resp.data.forging);
+            if (!pass || pass.length > 100){
+            }
+            else{
+                $http.get("/api/unlock", { params : { secretPhrase : pass }})
+                    .then(function (resp) {
+                        if (resp.data.success) {
+                            userService.setData(resp.data.address, resp.data.publickey, resp.data.balance, resp.data.unconfirmedBalance, resp.data.effectiveBalance);
+                            userService.setForging(resp.data.forging);
 
-                        $state.go('main.account');
-                    } else {
-                        alert("Something wrong. Restart server please.");
-                    }
-                });
+                            $state.go('main.account');
+                        } else {
+                            alert("Something wrong. Restart server please.");
+                        }
+                    });
+            }
         }
 }]);
