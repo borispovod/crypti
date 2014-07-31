662952f260e2bb35920165c028ad21f116468cc2
crypti <boris@crypti.me>
2014-07-31 12:40:38 +0400

    fixed error with verify generation signature
    Former-commit-id: 0b6566ea5ee4af9ed33cae3cd4ba5bdd03f7ea65

--- p2p/index.js
+++ p2p/index.js
@@ -5,6 +5,4 @@ module.exports.initRoutes = function (app) {
 }
 
 module.exports.peerprocessor = require("./peerprocessor.js");
-module.exports.p2p = require("./seed.js");
 module.exports.peer = require("./peer.js");
-//module.exports.socket = require("./socket.js");
