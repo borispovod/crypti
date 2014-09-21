c0b523fa2d05ce4fb1eb3d6119c5ac79b33b7344
Boris Povod <borispovod@MacBook-Pro-Boris.local>
2014-09-21 07:54:16 +0400

    tesnet functional
    Former-commit-id: 0e91bee0124b292a04a353cac2a4ec2e609e3e2d [formerly 4278f1f0722ec56d6b88d0b04c9627b2cb0e75d3]
    Former-commit-id: fda7de0d169dcbd46dd51bf31391f3b63f4e290c

--- block/genesisblock.js
+++ block/genesisblock.js
@@ -2,90 +2,18 @@ module.exports = {
     creatorId : "6945368540453891295C",
     publicKey : "02da206852581687ecf336f74d57a025483304ae5b6340caa0a724e9aec32470",
     generatorPublicKey: "9e51284be9f60a367d57b8d9dc40fb7a1e95cdf9c4ba249f4e96809fa05d5982",
-    requestGeneratorPublicKey : "c3843e2ece2ee743178972850e6282c023cfa699589c1b0ead423ff202d4bd69",
+    requestGeneratorPublicKey : "631b91fa537f74e23addccd30555fbc7729ea267c7e0517cbf1bfcc46354abc3",
     requestSignature: "9443e8c88af9fcbba606f9c9c7e4ffae3cab331db6ae2e2b7c0813659eddb1cdf9b7a916fbf1377713ad5292d4ee2b828cdd013c5d41619ce4fad74ab3f9ee0d",
-    blockId : "9306893212243057044",
+    blockId : "10910396031294105665",
     sender : "614c27657de2069fe27aa208f317c6e14ba9d6cd0c23b1ff9506228109ac61a1",
     amount : 99999999,
-    blockSignature: "a4c2a450de15938c94a4fd43bae9760c6183a4036debd0a98633a9080690e01213bfdcfd3f4101571189f9479773c250d421cdf09a51c78cc89ef44b6080c90e",
+    blockSignature: "3b0152cb9cf6a68a2fa0e28aa3449ccdc698518b8858ce62ba315b5e433c2cb75295e0528b2d5151b5764195c6f8bdff75c161c4bbe919074d0aba22b102a90a",
     transactions : [
         {
-            amount: 81408339.63,
+            amount: 100000000,
             fee : 0,
-            recipient : "983470101446368718C",
-            signature : "fda0e68e7d15310652db307f385676a86f543649cba17a71f6e630c9caa7394e8687fe2f185e50bad68dc2ab908703a280f564ddda86b599e4f3a4420ed89208"
-        },
-        {
-            amount: 515890.4722,
-            fee : 0,
-            recipient: "9082344706491735147C",
-            signature : "8c2e267a8005a09882dfa710b304cc1da48431cae35acfd8a72e60ab2ee1bc693c0f30cf17dba1de9d2e569a7e41ff35a604b4b16bc34739e3e36f080fc0f106"
-        },
-        {
-            amount: 1679818.39,
-            fee : 0,
-            recipient: "14424998199295226455C",
-            signature: "0ba06e92f96dc8f80d5b5724d26af377473a798003dfa859b992dfc6f9088b18a559615ef48376d403532c15bfaa0d3855d61660c4c28e906f7ca0dfc52e180f"
-        },
-        {
-            amount: 93803.03463,
-            fee : 0,
-            recipient: "2107455136554603860C",
-            signature: "1db798b6e650ab1247015d6dfe36258c68d00dcd0e7cb516b1bcc7846bc06eaa754f101f10655cafe7f49cc45cfa9e5fb9640d0f4a457f3f21dba305051ff507"
-        },
-        {
-            amount : 150879.2989,
-            fee : 0,
-            recipient : "2561018029680217600C",
-            signature: "156397b56951798be91fe39ed3bf15070e73169234bc77ca9f4545e330ea3e84bdfb4cfdbdb9e7d9d89eb596c348f810554b3dd34066cc2960beb1edf946890a"
-        },
-        {
-            amount : 1151269.179,
-            fee : 0,
-            recipient : "3512982625848417766C",
-            signature : "10022677df4ede8dbc9872dece417f3a96f118aa9c5ced6dceb956756d95b0e8ea69d244300504d7ec6e2fd9b32a8b541abf392313b88c40a8ec36f7596b9a0e"
-        },
-        {
-            amount : 11998999.99527,
-            fee : 0,
-            recipient : "15292791051968645847C",
-            signature : "7c8a23e6a9e955e098543903ab111ad15349a3335c15410bb90dbe9df327fa500a383af863321a1ae8203e5029660b6b449bbec0950dadb2fdf33aace2ce580b"
-        },
-        {
-            amount : 875000,
-            fee : 0,
-            recipient : "2306025133713822857C",
-            signature : "36fd2faf54b50757af6ace4ae27057ea890e61bdd7e0f5f619c1e5d7f260514a06caad3963868735fd3ad21e3888c4c4aa97469e02998d0f81b2c1cd16c73f0e"
-        },
-        {
-            amount : 1000,
-            fee : 0,
-            recipient : "14912045560994838943C",
-            signature : "6bfd2ecabb29b2d9ef19428baeed977f39a169229a0142db6c171f018bc364a4f0cd63c42fa254a42e0408eeee181fe81634a9fe6c54ee3d51f239ffc8bc3502"
-        },
-        {
-            amount: 875000,
-            fee : 0,
-            recipient : "706095399640310584C",
-            signature : "47293fd73d6db6183527eee37eb6ded5fe0f8e066de0bb2074931879f3ccc6edac69d3ef58bc3e334fd052cbb28eee68731c00257d71c5a780542b8a0af2270d"
-        },
-        {
-            amount : 375000,
-            fee : 0,
-            recipient : "8742596963146131725C",
-            signature : "1ef2a6a9757d5d5ac4b5b3a19d05603b5f382c0ef5bed4b338bdbac82045ee504a264ff9282d1ec1e888e669d3742842dfb2f1647de27bdbac522d7d58b7df01"
-        },
-        {
-            amount : 375000,
-            fee : 0,
-            recipient : "6127372741261621508C",
-            signature : "0543dcfb170b07f2b74863e9659d3ca57f5e9c9807fd9d86790cec76bc92ded66ca710fe16489cf11ffcc5a7e5ae538bd0b3477b52863164b50d600792e8ca0d"
-        },
-        {
-            amount : 500000,
-            fee : 0,
-            recipient : "4530343368638932416C",
-            signature : "cc577a030dad8d95145c8efd734b5db9e38257636a86783562e25e0de90563c009f8fd32c142273e28b5381f988176b1a814f4f3be7552b9cc7b24410ef3fa01"
+            recipient : "2334212999465599568C",
+            signature : "c6773814cdc0e528efb5b3e2892a6a7d92e619c7d6f4a635298c8364b52eabfe5a94d874bfeee2a49df365e231ecb4dd189edae004874820ddd1db9689d36202"
         }
     ]
 }
