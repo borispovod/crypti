e54348bc35d6b908946f9da586b6a01054e01f67
landgraf.paul <landgraf.paul@gmail.com>
2014-11-22 18:16:18 +0100

    minor fix
    Former-commit-id: 1e2ba3b3ce6d6d39177defae578b16eb0511ae05

--- test/transport.js
+++ test/transport.js
@@ -3,8 +3,10 @@ var	async = require('async');
 function Module(modules, cb) {
 	var public = {};
 
-	modules.blockchain.open(function(err, blockchain){
-		console.log(blockchain.length);
+	modules.models.blocks.open(function(err, blockchain){
+		if (!err) {
+			console.log(blockchain.blocks.length);
+		}
 	})
 
 	cb(null, public);
