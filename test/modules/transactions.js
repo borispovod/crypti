0f80b5a3d1633c6ff31710dd5a1cd9dc2f69820f
Boris Povod <boris@crypti.me>
2014-11-25 04:59:16 +0400

    added transactions helper
    Former-commit-id: 59f4ee0c0a336fa13449fca0b4354db39a132346 [formerly 01d088baf5f8329d605e2cc2d14e2d55a948f8ed]
    Former-commit-id: 6b333263600d4ccee9a6309bce53cf96ba26fff7

--- test/modules/transactions.js
+++ test/modules/transactions.js
@@ -1,71 +1,7 @@
+var transactionHelper = require('../helpers/transaction.js');
+
 // private
 var modules, library;
-
-var fixedPoint = Math.pow(10, 8);
-
-// get valid transaction fee, if we need to get fee for block generator, use isGenerator = true
-function getTransactionFee(transaction, isGenerator) {
-    var fee = -1;
-
-    switch (transaction.type) {
-        case 0:
-            switch (transaction.subtype) {
-                case 0:
-                    fee = transaction.fee;
-                    break;
-            }
-            break;
-
-        case 1:
-            switch (transaction.subtype) {
-                case 0:
-                    if (transaction.fee >= 2) {
-                        if (transaction.fee % 2 != 0) {
-                            var tmp = parseInt(transaction.fee / 2);
-
-                            if (isGenerator) {
-                                fee = transaction.fee - tmp;
-                            } else {
-                                fee = tmp;
-                            }
-                        } else {
-                            fee = transaction.fee / 2;
-                        }
-                    } else {
-                        if (isGenerator) {
-                            fee = transaction.fee;
-                        } else {
-                            fee = 0;
-                        }
-                    }
-                    break;
-            }
-            break;
-
-        case 2:
-            switch (transaction.subtype) {
-                case 0:
-                    fee = transaction.fee;
-                    break;
-            }
-            break;
-
-        case 3:
-            switch (transaction.subtype) {
-                case 0:
-                    fee = 100 * fixedPoint;
-                    break;
-            }
-            break;
-    }
-
-    if (fee == -1) {
-        throw new Error("Invalid transaction type: " + t.id);
-    }
-
-    return fee;
-}
-
 
 function Transactions(cb, scope) {
     library = scope;
@@ -109,7 +45,7 @@ Transactions.prototype.apply = function (transaction, cb) {
                         }
 
                         // need to calculate fee
-                        amount = transaction.amount + getTransactionFee(transaction, false);
+                        amount = transaction.amount + transactionHelper.getTransactionFee(transaction, false);
                         companyCreator.addToUnconfirmedBalance(amount);
                         companyCreator.addToBalance(amount);
 
