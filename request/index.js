deaaaba0ba4545d01d8d308cb3028049c8dd409d
landgraf.paul <landgraf.paul@gmail.com>
2014-11-25 22:41:45 +0100

    clear folder
    Former-commit-id: 3f751deecae865747d8e03df235a59a61a2bc32e

--- request/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    request : require('./request.js'),
-    requestprocessor : require('./requestprocessor.js'),
-    requestconfirmation : require('./requestconfirmation.js')
-}
