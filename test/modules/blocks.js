8bd8703ea2141e9b3ffa90b379eeb90399cd8cb1
landgraf.paul <landgraf.paul@gmail.com>
2014-11-25 01:01:19 +0100

    update load blocks; update server
    Former-commit-id: d2fa5eed7eecbb2a2a6e1ad301d61f5960f9bb28

--- test/modules/blocks.js
+++ test/modules/blocks.js
@@ -25,39 +25,49 @@ function Blocks(cb, scope) {
 				library.db.all(
 					"SELECT " +
 					"b.id b_id, b.version b_version, b.timestamp b_timestamp, b.height b_height, b.previousBlock b_previousBlock, b.nextBlock b_nextBlock, b.numberOfRequests b_numberOfRequests, b.numberOfTransactions b_numberOfTransactions, b.numberOfConfirmations b_numberOfConfirmations, b.totalAmount b_totalAmount, b.totalFee b_totalFee, b.payloadLength b_payloadLength, b.requestsLength b_requestsLength, b.confirmationsLength b_confirmationsLength, b.payloadHash b_payloadHash, b.generatorPublicKey b_generatorPublicKey, b.generationSignature b_generationSignature, b.blockSignature b_blockSignature, " +
-					"t.id t_id, t.blockId t_blockId,  t.type t_type, t.subtype t_subtype, t.timestamp t_timestamp, t.senderPublicKey t_senderPublicKey, t.sender t_sender, t.recipientId t_recipientId, t.amount t_amount, t.fee t_fee, t.signature t_signature, t.signSignature t_signSignature, " +
+					"t.id t_id, t.blockId t_blockId, t.type t_type, t.subtype t_subtype, t.timestamp t_timestamp, t.senderPublicKey t_senderPublicKey, t.sender t_sender, t.recipientId t_recipientId, t.amount t_amount, t.fee t_fee, t.signature t_signature, t.signSignature t_signSignature, " +
 					"s.id s_id, s.transactionId s_transactionId, s.timestamp s_timestamp, s.publicKey s_publicKey, s.generatorPublicKey s_generatorPublicKey, s.signature s_signature, s.generationSignature s_generationSignature, " +
 					"c.id c_id, c.transactionId c_transactionId, c.name c_name, c.description c_description, c.domain c_domain, c.email c_email, c.timestamp c_timestamp, c.generatorPublicKey c_generatorPublicKey, c.signature c_signature " +
 					"FROM blocks as b " +
 					"left outer join trs as t on blockId=b.id " +
 					"left outer join signatures as s on s.transactionId=t.id " +
 					"left outer join companies as c on c.transactionId=t.id " +
-					"ORDER BY height " +
+					"ORDER BY b.rowid, t.rowid, s.rowid, c.rowid " +
 					"", cb);
 			})
 		}
 	}, function (err, scope) {
 		if (!err) {
-			blocks = {};
-			lastBlock = scope.blocks.length && scope.blocks[0];
-			for (var i = 0, length = scope.blocks.length; i < length; i++) {
+			blocks = [];
+			lastBlock = scope.blocks.length && scope.blocks[scope.blocks.length - 1];
 
+			var prevBlockId = null, prevTransactionId = null, b_index, t_index;
+			for (var i = 0, length = scope.blocks.length; i < length; i++) {
 				var block = blockHelper.getBlock(scope.blocks[i]);
 				if (block) {
-					!blocks[block.id] && (blocks[block.id] = block);
+					if (prevBlockId != block.id) {
+						blocks.push(block);
+						b_index = blocks.length - 1;
+						prevBlockId = block.id;
+					}
+
 					var transaction = blockHelper.getTransaction(scope.blocks[i]);
 					if (transaction) {
-						!block.transactions && (block.transactions = []);
-						block.transactions.push(transaction);
+						!blocks[b_index].transactions && (blocks[b_index].transactions = []);
+						if (prevTransactionId != transaction.id) {
+							blocks[b_index].transactions.push(transaction);
+							t_index = blocks[b_index].transactions.length - 1;
+							prevTransactionId = transaction.id;
+						}
 						var signature = blockHelper.getSignature(scope.blocks[i]);
 						if (signature) {
-							!transaction.signatures && (transaction.signatures = []);
-							transaction.signatures.push(signature);
+							!blocks[b_index].transactions[t_index].signatures && (blocks[b_index].transactions[t_index].signatures = []);
+							blocks[b_index].transactions[t_index].signatures.push(signature);
 						}
 						var company = blockHelper.getSignature(scope.blocks[i]);
 						if (company) {
-							!transaction.companies && (transaction.companies = []);
-							transaction.companies.push(company);
+							!blocks[b_index].transactions[t_index].companies && (blocks[b_index].transactions[t_index].companies = []);
+							blocks[b_index].transactions[t_index].companies.push(company);
 						}
 					}
 				}
@@ -87,31 +97,31 @@ Blocks.prototype.verifySignature = function (block) {
 }
 
 Blocks.prototype.verifyGenerationSignature = function (block) {
-    // maybe need to add requests to see how it's working
-    var previousBlock = blocks[block.previousBlock];
-    if (previousBlock == null) {
-        return false;
-    }
+	// maybe need to add requests to see how it's working
+	var previousBlock = blocks[block.previousBlock];
+	if (previousBlock == null) {
+		return false;
+	}
 
-    var hash = crypto.createHash('sha256').update(previousBlock.generationSignature).update(block.generatorPublicKey);
-    var generationSignatureHash = hash.digest();
+	var hash = crypto.createHash('sha256').update(previousBlock.generationSignature).update(block.generatorPublicKey);
+	var generationSignatureHash = hash.digest();
 
-    var r = ed.Verify(generationSignatureHash, block.generationSignature, block.generatorPublicKey);
-    if (!r) {
-        return false;
-    }
+	var r = ed.Verify(generationSignatureHash, block.generationSignature, block.generatorPublicKey);
+	if (!r) {
+		return false;
+	}
 
-    var generator = modules.accounts.getAccountByPublicKey(block.generatorPublicKey);
+	var generator = modules.accounts.getAccountByPublicKey(block.generatorPublicKey);
 
-    if (!generator) {
-        return false;
-    }
+	if (!generator) {
+		return false;
+	}
 
-    if (generator.getEffectiveBalance() < 1000 * constants.numberLength) {
-        return false;
-    }
+	if (generator.getEffectiveBalance() < 1000 * constants.numberLength) {
+		return false;
+	}
 
-    return true;
+	return true;
 }
 
 Blocks.prototype.getAll = function () {
