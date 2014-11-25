3f751deecae865747d8e03df235a59a61a2bc32e
landgraf.paul <landgraf.paul@gmail.com>
2014-11-25 22:41:45 +0100

    clear folder

--- logger/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    logger : require("./logger.js")
-}
--- p2p/index.js
+++
@@ -1,8 +0,0 @@
-var p2proutes = require('./p2proutes.js');
-
-module.exports.initRoutes = function (app) {
-    p2proutes(app);
-}
-
-module.exports.peerprocessor = require("./peerprocessor.js");
-module.exports.peer = require("./peer.js");
