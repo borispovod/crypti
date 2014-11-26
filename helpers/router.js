0ef6f6a446f7b7dd0ad24b72080d1a0326786000
landgraf.paul <landgraf.paul@gmail.com>
2014-11-26 20:10:10 +0100

    update api
    Former-commit-id: 18f4bcb4c7f53c8390499a055be4b8fa6804a6fa

--- helpers/router.js
+++ helpers/router.js
@@ -1,19 +1,21 @@
-var router = require('express').Router();
+module.exports = function () {
+	var router = require('express').Router();
 
-router.param(function (name, fn) {
-	if (fn instanceof RegExp) {
-		return function (req, res, next, val) {
-			var captures;
-			if (captures = fn.exec(String(val))) {
-				req.params[name] = captures.length == 1 ? captures[0] : captures;
-				next();
-			} else {
-				next('route');
+	router.param(function (name, fn) {
+		if (fn instanceof RegExp) {
+			return function (req, res, next, val) {
+				var captures;
+				if (captures = fn.exec(String(val))) {
+					req.params[name] = captures.length == 1 ? captures[0] : captures;
+					next();
+				} else {
+					next('route');
+				}
 			}
 		}
-	}
-});
+	});
 
-router.param('id', /^[0-9]+$/);
+	router.param('id', /^[0-9]+$/);
 
-module.exports = router;
+	return router;
+};
