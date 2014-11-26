acee558c33512f74bd08c02c0c039728012c56de
landgraf.paul <landgraf.paul@gmail.com>
2014-11-26 20:10:10 +0100

    update api
    Former-commit-id: 253ba0b78f4111e6c9dc02670065b3df6487a411 [formerly 18f4bcb4c7f53c8390499a055be4b8fa6804a6fa]
    Former-commit-id: 0ef6f6a446f7b7dd0ad24b72080d1a0326786000

--- modules/accounts.js
+++ modules/accounts.js
@@ -1,5 +1,6 @@
 var crypto = require('crypto'),
-    bignum = require('bignum');
+	bignum = require('bignum');
+var Router = require('../helpers/router.js');
 
 //private
 var modules, library;
@@ -7,89 +8,99 @@ var accounts;
 
 //public
 function Account(address, balance, unconfirmedBalance) {
-    this.address = address;
-    this.balance = balance || 0;
-    this.unconfirmedBalance = unconfirmedBalance || 0;
+	this.address = address;
+	this.balance = balance || 0;
+	this.unconfirmedBalance = unconfirmedBalance || 0;
 }
 
 Account.prototype.addToBalance = function (amount) {
-    this.balance += amount;
+	this.balance += amount;
 }
 
 Account.prototype.addToUnconfirmedBalance = function (amount) {
-    this.unconfirmedBalance += amount;
+	this.unconfirmedBalance += amount;
 }
 
 Account.prototype.setBalance = function (balance) {
-    this.balance = balance;
+	this.balance = balance;
 }
 
 Account.prototype.setUnconfirmedBalance = function (unconfirmedBalance) {
-    this.unconfirmedBalance = unconfirmedBalance;
+	this.unconfirmedBalance = unconfirmedBalance;
 }
 
 function Accounts(cb, scope) {
-    library = scope;
-    accounts = {};
+	library = scope;
+	accounts = {};
 
-    setImmediate(function () {
-        cb(null, this);
-    }.bind(this));
+	var router = new Router();
+
+	router.get('/open', function (req, res) {
+		return res.json({});
+	});
+
+	router.get('/getBalance', function (req, res) {
+		return res.json({});
+	});
+
+	library.app.use('/accounts', router);
+
+	setImmediate(cb, null, this);
 }
 
 Accounts.prototype.addAccount = function (account) {
-    if (!accounts[account.address]) {
-        accounts[account.address] = account;
-    }
+	if (!accounts[account.address]) {
+		accounts[account.address] = account;
+	}
 }
 
 Accounts.prototype.getAccount = function (id) {
-    return accounts[id];
+	return accounts[id];
 }
 
 Accounts.prototype.getAccountByPublicKey = function (publicKey) {
-    var address = this.getAddressByPublicKey(publicKey);
-    return this.getAccount(address);
+	var address = this.getAddressByPublicKey(publicKey);
+	return this.getAccount(address);
 }
 
 Accounts.prototype.getAddressByPublicKey = function (publicKey) {
-    var publicKeyHash = crypto.createHash('sha256').update(publicKey).digest();
-    var temp = new Buffer(8);
-    for (var i = 0; i < 8; i++) {
-        temp[i] = publicKeyHash[7-i];
-    }
+	var publicKeyHash = crypto.createHash('sha256').update(publicKey).digest();
+	var temp = new Buffer(8);
+	for (var i = 0; i < 8; i++) {
+		temp[i] = publicKeyHash[7 - i];
+	}
 
-    var address = bignum.fromBuffer(temp).toString() + "C";
-    return address;
+	var address = bignum.fromBuffer(temp).toString() + "C";
+	return address;
 }
 
 Accounts.prototype.getAccountOrCreate = function (addressOrPublicKey) {
-    var account, address;
+	var account, address;
 
-    if (typeof(addressOrPublicKey) == 'string') {
-        address = addressOrPublicKey;
-        account = this.getAccount(address);
-    } else {
-        address = this.getAddressByPublicKey(addressOrPublicKey);
-        account = this.getAccount(address);
-    }
+	if (typeof(addressOrPublicKey) == 'string') {
+		address = addressOrPublicKey;
+		account = this.getAccount(address);
+	} else {
+		address = this.getAddressByPublicKey(addressOrPublicKey);
+		account = this.getAccount(address);
+	}
 
-    if (!account) {
-        account = new Account(address);
-        this.addAccount(account);
+	if (!account) {
+		account = new Account(address);
+		this.addAccount(account);
 
-        return account;
-    } else {
-        return account;
-    }
+		return account;
+	} else {
+		return account;
+	}
 }
 
 Accounts.prototype.getAllAccounts = function () {
-    return accounts;
+	return accounts;
 }
 
 Accounts.prototype.run = function (scope) {
-    modules = scope;
+	modules = scope;
 }
 
 module.exports = Accounts;
