828ebca805bc4396a73c82e102d416782dd8b4eb
landgraf.paul <landgraf.paul@gmail.com>
2014-11-27 15:17:39 +0100

    add api ref
    Former-commit-id: 425bcea85722f22f8060a4f0bd17180ec2fae2ca

--- public/js/controllers/loadingController.js
+++ public/js/controllers/loadingController.js
@@ -2,7 +2,7 @@ webApp.controller("loadingController", ["$scope", "$http", "$interval", "$window
     $scope.height = null;
 
     $scope.getHeight = function () {
-        $http.get("/blocks/status")
+        $http.get("/api/blocks/status")
             .then(function (resp) {
                 if (resp.data.success) {
                     if (!resp.data.loaded) {
