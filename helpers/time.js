ca1fc1854be217a604809fd2fe7d6ca9d1d3944f
Boris Povod <boris@crypti.me>
2014-12-01 06:05:05 +0400

    processing fee for forger & fee level
    Former-commit-id: 484853755ab3420a638a3a364717a55bf8d87dd9

--- helpers/time.js
+++ helpers/time.js
@@ -1,3 +1,23 @@
-module.exports = {
 
+function getEpochTime(time) {
+	var d = new Date(Date.UTC(2014, 4, 2, 0, 0, 0, 0));
+	var t = d.getTime();
+	return parseInt((time - t) / 1000);
+}
+
+function getNow() {
+	return getEpochTime(new Date().getTime());
+}
+
+function epochTime() {
+	var d = new Date(Date.UTC(2014, 4, 2, 0, 0, 0, 0));
+	var t = parseInt(d.getTime() / 1000);
+
+	return t;
+}
+
+module.exports = {
+	getEpochTime: getEpochTime,
+	epochTime : epochTime,
+	getNow : getNow
 }
