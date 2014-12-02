7a2552363bf03c224c080d12080be9ff0c30c515
landgraf.paul <landgraf.paul@gmail.com>
2014-12-02 01:59:31 +0100

    update ui
    Former-commit-id: 0750037da4280925b40e0091a791cc7f630f2dc6 [formerly 506224a764663e80823b0dc5155b4b7eac7e118f]
    Former-commit-id: ae5f5446d96be6383c534672751a325c56f6c994

--- public/js/services/userService.js
+++ public/js/services/userService.js
@@ -1,19 +1,22 @@
 webApp.service('userService', function () {
-    this.setData = function (address, publicKey, balance, unconfirmedBalance, effectiveBalance) {
-        this.address = address;
-        this.publicKey = publicKey;
-        this.balance = balance / 100000000;
-        this.unconfirmedBalance = unconfirmedBalance / 100000000;
-        this.effectiveBalance = effectiveBalance / 100000000;
-        this._balance = balance;
-        this._unconfirmedBalance = unconfirmedBalance;
-    }
+	this.setData = function (address, publicKey, balance, unconfirmedBalance, effectiveBalance) {
+		console.log('userService.params', address, publicKey, balance, unconfirmedBalance, effectiveBalance)
+		this.address = address;
+		this.publicKey = publicKey;
+		this.balance = balance / 100000000;
+		this.unconfirmedBalance = unconfirmedBalance / 100000000;
+		this.effectiveBalance = effectiveBalance / 100000000;
+		this._balance = balance;
+		this._unconfirmedBalance = unconfirmedBalance;
+		console.log('userService.create', this)
 
-    this.setForging = function (forging) {
-        this.forging = forging;
-    }
+	}
 
-    this.setSecondPassphrase = function (secondPassPhrase) {
-        this.secondPassphrase = secondPassPhrase;
-    }
+	this.setForging = function (forging) {
+		this.forging = forging;
+	}
+
+	this.setSecondPassphrase = function (secondPassPhrase) {
+		this.secondPassphrase = secondPassPhrase;
+	}
 });
