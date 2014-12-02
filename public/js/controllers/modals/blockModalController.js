ae5f5446d96be6383c534672751a325c56f6c994
landgraf.paul <landgraf.paul@gmail.com>
2014-12-02 01:59:31 +0100

    update ui
    Former-commit-id: 506224a764663e80823b0dc5155b4b7eac7e118f

--- public/js/controllers/modals/blockModalController.js
+++ public/js/controllers/modals/blockModalController.js
@@ -1,7 +1,6 @@
 webApp.controller('blockModalController', ["$scope", "blockModal", function ($scope, blockModal) {
     $scope.addresses = $scope.block.addresses;
     $scope.transactions = $scope.block.transactions;
-    console.log($scope.transactions);
 
     $scope.close = function () {
         blockModal.deactivate();
