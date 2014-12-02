9f119ec1316600ef977d2cafb71e0d07e0aff924
Boris Povod <boris@crypti.me>
2014-12-02 12:43:04 +0400

    load transactions of block
    Former-commit-id: 6860f8fa034780a55861d7c955efb1f3f06a1e58

--- public/js/controllers/modals/blockModalController.js
+++ public/js/controllers/modals/blockModalController.js
@@ -1,6 +1,20 @@
-webApp.controller('blockModalController', ["$scope", "blockModal", function ($scope, blockModal) {
+webApp.controller('blockModalController', ["$scope", "$http", "blockModal", function ($scope, $http, blockModal) {
+	$scope.loading = true;
+
+	$scope.getTransactionsOfBlock = function (blockId) {
+		$http.get("/api/transactions/", { params : { blockId : blockId }})
+			.then(function (resp) {
+				$scope.transactions = resp.data.transactions;
+				$scope.loading = false;
+			});
+	};
+
+	$scope.getTransactionsOfBlock($scope.block.id);
+
+	/*
     $scope.addresses = $scope.block.addresses;
     $scope.transactions = $scope.block.transactions;
+    */
 
     $scope.close = function () {
         blockModal.deactivate();
