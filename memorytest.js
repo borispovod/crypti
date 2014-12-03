c9ce679cb750bfe25273fe833894b130619ba7f1
Boris Povod <boris@crypti.me>
2014-12-03 21:22:49 +0400

    changed memory test

--- memorytest.js
+++ memorytest.js
@@ -1,23 +1,29 @@
 var sqlite3 = require('./helpers/db.js');
 sqlite3.connect('./blockchain.db', function (err, db) {
-	db.all(
-		"SELECT " +
-		"b.id b_id, b.version b_version, b.timestamp b_timestamp, b.height b_height, b.previousBlock b_previousBlock, b.nextBlock b_nextBlock, b.numberOfRequests b_numberOfRequests, b.numberOfTransactions b_numberOfTransactions, b.numberOfConfirmations b_numberOfConfirmations, b.totalAmount b_totalAmount, b.totalFee b_totalFee, b.payloadLength b_payloadLength, b.requestsLength b_requestsLength, b.confirmationsLength b_confirmationsLength, b.payloadHash b_payloadHash, b.generatorPublicKey b_generatorPublicKey, b.generationSignature b_generationSignature, b.blockSignature b_blockSignature, " +
-		"t.id t_id, t.blockId t_blockId, t.type t_type, t.subtype t_subtype, t.timestamp t_timestamp, t.senderPublicKey t_senderPublicKey, t.senderId t_senderId, t.recipientId t_recipientId, t.amount t_amount, t.fee t_fee, t.signature t_signature, t.signSignature t_signSignature, c_t.generatorPublicKey t_companyGeneratorPublicKey, " +
-		"s.id s_id, s.transactionId s_transactionId, s.timestamp s_timestamp, s.publicKey s_publicKey, s.generatorPublicKey s_generatorPublicKey, s.signature s_signature, s.generationSignature s_generationSignature, " +
-		"c.id c_id, c.transactionId c_transactionId, c.name c_name, c.description c_description, c.domain c_domain, c.email c_email, c.timestamp c_timestamp, c.generatorPublicKey c_generatorPublicKey, c.signature c_signature, " +
-		"cc.id cc_id, cc.blockId cc_blockId, cc.companyId cc_companyId, cc.verified cc_verified, cc.timestamp cc_timestamp, cc.signature cc_signature " +
-		"FROM (select * from blocks) as b " +
-		"left outer join trs as t on t.blockId=b.id " +
-		"left outer join signatures as s on s.transactionId=t.id " +
-		"left outer join companies as c on c.transactionId=t.id " +
-		"left outer join companies as c_t on c_t.address=t.recipientId " +
-		"left outer join companyconfirmations as cc on cc.blockId=b.id " +
-		"ORDER BY b.height, t.rowid, s.rowid, c.rowid, cc.rowid " +
-		"", function (err, rows) {
-			console.log(err, rows && rows.length);
-			db.close();
+
+	db.serialize(function () {
+		var st = db.prepare(
+				"SELECT " +
+				"b.id b_id, b.version b_version, b.timestamp b_timestamp, b.height b_height, b.previousBlock b_previousBlock, b.nextBlock b_nextBlock, b.numberOfRequests b_numberOfRequests, b.numberOfTransactions b_numberOfTransactions, b.numberOfConfirmations b_numberOfConfirmations, b.totalAmount b_totalAmount, b.totalFee b_totalFee, b.payloadLength b_payloadLength, b.requestsLength b_requestsLength, b.confirmationsLength b_confirmationsLength, b.payloadHash b_payloadHash, b.generatorPublicKey b_generatorPublicKey, b.generationSignature b_generationSignature, b.blockSignature b_blockSignature, " +
+				"t.id t_id, t.blockId t_blockId, t.type t_type, t.subtype t_subtype, t.timestamp t_timestamp, t.senderPublicKey t_senderPublicKey, t.senderId t_senderId, t.recipientId t_recipientId, t.amount t_amount, t.fee t_fee, t.signature t_signature, t.signSignature t_signSignature, c_t.generatorPublicKey t_companyGeneratorPublicKey, " +
+				"s.id s_id, s.transactionId s_transactionId, s.timestamp s_timestamp, s.publicKey s_publicKey, s.generatorPublicKey s_generatorPublicKey, s.signature s_signature, s.generationSignature s_generationSignature, " +
+				"c.id c_id, c.transactionId c_transactionId, c.name c_name, c.description c_description, c.domain c_domain, c.email c_email, c.timestamp c_timestamp, c.generatorPublicKey c_generatorPublicKey, c.signature c_signature, " +
+				"cc.id cc_id, cc.blockId cc_blockId, cc.companyId cc_companyId, cc.verified cc_verified, cc.timestamp cc_timestamp, cc.signature cc_signature " +
+				"FROM (select * from blocks) as b " +
+				"left outer join trs as t on t.blockId=b.id " +
+				"left outer join signatures as s on s.transactionId=t.id " +
+				"left outer join companies as c on c.transactionId=t.id " +
+				"left outer join companies as c_t on c_t.address=t.recipientId " +
+				"left outer join companyconfirmations as cc on cc.blockId=b.id " +
+				"ORDER BY b.height, t.rowid, s.rowid, c.rowid, cc.rowid " +
+				""
+		);
+
+		st.all(function (err, row) {
+			console.log(err, row.length);
 		});
+	});
+
 });
 
 process.stdin.resume();
