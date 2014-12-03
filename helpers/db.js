40cc84762095b3a965606372cc488612cefef3b2
Boris Povod <boris@crypti.me>
2014-12-03 13:53:58 +0400

    forging screen & send transaction
    Former-commit-id: c2b8b965837e2272ad41c05a438091717182f98a

--- helpers/db.js
+++ helpers/db.js
@@ -6,7 +6,7 @@ module.exports.connect = function (connectString, cb) {
     // varchar(20) for ids, varchar(21) for addresses
     var sql = [
         "CREATE TABLE IF NOT EXISTS blocks (id VARCHAR(20) PRIMARY KEY, version INT NOT NULL, timestamp INT NOT NULL, height INT NOT NULL, previousBlock VARCHAR(21), nextBlock VARCHAR(21), numberOfRequests INT NOT NULL, numberOfTransactions INT NOT NULL, numberOfConfirmations INT NOT NULL, totalAmount BIGINT NOT NULL, totalFee BIGINT NOT NULL, payloadLength INT NOT NULL, requestsLength INT NOT NULL, confirmationsLength INT NOT NULL, payloadHash BINARY(32) NOT NULL, generatorPublicKey BINARY(32) NOT NULL, generationSignature BINARY(64) NOT NULL, blockSignature BINARY(64) NOT NULL, FOREIGN KEY (previousBlock) REFERENCES blocks(id), FOREIGN KEY (nextBlock) REFERENCES blocks(id) ON DELETE CASCADE)",
-        "CREATE TABLE IF NOT EXISTS trs (id VARCHAR(20) PRIMARY KEY, blockId VARCHAR(20) NOT NULL, type TINYINT NOT NULL, subtype TINYINT NOT NULL, timestamp INT NOT NULL, senderPublicKey BINARY(32) NOT NULL, sender VARCHAR(21) NOT NULL, recipientId VARCHAR(21) NOT NULL, amount BIGINT NOT NULL, fee BIGINT NOT NULL, signature BINARY(64) NOT NULL, signSignature BINARY(64), FOREIGN KEY(blockId) REFERENCES blocks(id) ON DELETE CASCADE)",
+        "CREATE TABLE IF NOT EXISTS trs (id VARCHAR(20) PRIMARY KEY, blockId VARCHAR(20) NOT NULL, type TINYINT NOT NULL, subtype TINYINT NOT NULL, timestamp INT NOT NULL, senderPublicKey BINARY(32) NOT NULL, senderId VARCHAR(21) NOT NULL, recipientId VARCHAR(21) NOT NULL, amount BIGINT NOT NULL, fee BIGINT NOT NULL, signature BINARY(64) NOT NULL, signSignature BINARY(64), FOREIGN KEY(blockId) REFERENCES blocks(id) ON DELETE CASCADE)",
         "CREATE TABLE IF NOT EXISTS requests (id VARCHAR(20) PRIMARY KEY, blockId VARCHAR(20) NOT NULL, address VARCHAR(21) NOT NULL, FOREIGN KEY(blockId) REFERENCES blocks(id) ON DELETE CASCADE)",
         "CREATE TABLE IF NOT EXISTS signatures (id VARCHAR(20) PRIMARY KEY, transactionId VARCHAR(20) NOT NULL, timestamp INT NOT NULL, publicKey BINARY(32) NOT NULL, generatorPublicKey BINARY(32) NOT NULL, signature BINARY(64) NOT NULL, generationSignature BINARY(64) NOT NULL, FOREIGN KEY(transactionId) REFERENCES trs(id) ON DELETE CASCADE)",
         "CREATE TABLE IF NOT EXISTS companies (id VARCHAR(20) PRIMARY KEY, transactionId VARCHAR(20) NOT NULL, address VARCHAR(21), name VARCHAR(20) NOT NULL, description VARCHAR(250) NOT NULL, domain TEXT, email TEXT NOT NULL, timestamp INT NOT NULL, generatorPublicKey BINARY(32) NOT NULL, signature BINARY(32) NOT NULL, FOREIGN KEY(transactionId) REFERENCES trs(id) ON DELETE CASCADE)",
@@ -14,7 +14,7 @@ module.exports.connect = function (connectString, cb) {
         "CREATE UNIQUE INDEX IF NOT EXISTS blocks_height ON blocks(height)",
         "CREATE INDEX IF NOT EXISTS blocks_generator_public_key ON blocks(generatorPublicKey)",
         "CREATE INDEX IF NOT EXISTS trs_block_id ON trs (blockId)",
-        "CREATE INDEX IF NOT EXISTS trs_sender_id ON trs(sender)",
+        "CREATE INDEX IF NOT EXISTS trs_sender_id ON trs(senderId)",
         "CREATE INDEX IF NOT EXISTS trs_recipient_id ON trs(recipientId)",
         "CREATE INDEX IF NOT EXISTS signatures_trs_id ON signatures(transactionId)",
 		"CREATE INDEX IF NOT EXISTS signatures_generator_public_key ON signatures(generatorPublicKey)",
