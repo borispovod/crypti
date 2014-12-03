09c06a295e444aa56e42a090a9d998bd214059b0
landgraf.paul <landgraf.paul@gmail.com>
2014-12-03 15:53:20 +0100

    update grunt-jscrambler
    Former-commit-id: 58afbf9dd0f40d26d73d6f2ddc7cc0d3e85b5b14 [formerly bd1015ba1de70fb020de32dc9b626133548f3bb4]
    Former-commit-id: 35a0256346ec30d277159fcc2b654463278a2750

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -52,28 +52,6 @@ module.exports = function (grunt) {
 					"static/js/app.js": files
 				}
 			}
-		},
-		jscrambler: {
-			main: {
-				files: [
-					{src: 'static/js/app.js', dest: './'},
-				],
-				options: {
-					keys: {
-						accessKey: '24F15B0087298FBDEE7E90FE0B14F34D33E12CE2',
-						secretKey: 'FC255E27922479D0D8FE40CFAE8FBA45DD08947A'
-					},
-					params: {
-						rename_local: '%DEFAULT%',
-						whitespace: '%DEFAULT%',
-						duplicate_literals: '%DEFAULT%',
-						function_reorder: '%DEFAULT%',
-						expiration_date: '2015/12/31',
-						dot_notation_elimination: '%DEFAULT%',
-						function_outlining: '%DEFAULT%'
-					}
-				}
-			}
 		}
 	});
 
@@ -81,10 +59,9 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks("grunt-contrib-cssmin");
 	grunt.loadNpmTasks("grunt-contrib-less");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-jscrambler");
 
 	// Default task.
 	grunt.registerTask("default", ["less", "cssmin", "concat"]);
 	// Release task
-	grunt.registerTask("release", ["default", "uglify:release", "jscrambler"]);
+	grunt.registerTask("release", ["default", "uglify:release"]);
 };
