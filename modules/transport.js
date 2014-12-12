b1a8009a2927a3ac483e430563bf469242bd91ac
landgraf.paul <landgraf.paul@gmail.com>
2014-12-12 01:58:48 +0100

    add headers, update basic auth, update peers

--- modules/transport.js
+++ modules/transport.js
@@ -25,13 +25,13 @@ Transport.prototype.run = function (scope) {
 		os: modules.system.getOS(),
 		version: modules.system.getVersion(),
 		port: modules.system.getPort(),
-		sharePort: modules.system.getSharePort()
+		'share-port': modules.system.getSharePort()
 	}
 }
 
-function _request(url, method, data, cb) {
+function _request(peer, api, method, data, cb) {
 	var req = {
-		url: url,
+		url: 'http://' + ip.fromLong(peer.ip) + ':' + peer.port + '/peer' + api,
 		method: method,
 		json: true
 	};
@@ -44,10 +44,18 @@ function _request(url, method, data, cb) {
 	if (cb) {
 		request(req, function (err, response, body) {
 			if (!err && response.statusCode == 200) {
-				console.log(url, method, data, body)
+				modules.peer.update({
+					ip: peer.ip,
+					port: Number(response.headers['port']),
+					state: 2,
+					os: response.headers['os'],
+					sharePort: Number(!!response.headers['share-port']),
+					version: response.headers['version']
+				});
 				cb(null, body);
 			} else {
-				cb(err || response)
+				modules.peer.state(peer.ip, peer.port, 1);
+				cb(err || response);
 			}
 		});
 	} else {
@@ -65,7 +73,7 @@ Transport.prototype.broadcast = function (peersCount, method, data, cb) {
 		if (!err) {
 			async.eachLimit(peers, 3, function (peer, cb) {
 				// not need to check peer is offline or online, just send.
-				_request('http://' + ip.fromLong(peer.ip) + ':' + peer.port + '/peer' + method, "POST", data);
+				_request(peer, method, "POST", data);
 				setImmediate(cb);
 			}, function () {
 				cb && cb(null, {body: null, peer: peers});
@@ -80,7 +88,7 @@ Transport.prototype.getFromRandomPeer = function (method, cb) {
 	modules.peer.list(1, function (err, peers) {
 		if (!err && peers.length) {
 			var peer = peers[0];
-			_request('http://' + ip.fromLong(peer.ip) + ":" + peer.port + "/peer" + method, "GET", undefined, function (err, body) {
+			_request(peer, method, "GET", undefined, function (err, body) {
 				cb(err, {body: body, peer: peer});
 			});
 		} else {
@@ -90,7 +98,7 @@ Transport.prototype.getFromRandomPeer = function (method, cb) {
 }
 
 Transport.prototype.getFromPeer = function (peer, method, cb) {
-	_request('http://' + ip.fromLong(peer.ip) + ":" + peer.port + "/peer" + method, "GET", undefined, function (err, body) {
+	_request(peer, method, "GET", undefined, function (err, body) {
 		cb(err, {body: body, peer: peer});
 	});
 }
@@ -115,6 +123,8 @@ Transport.prototype.onBlockchainReady = function () {
 	});
 
 	router.get("/blocks/ids", function (req, res) {
+		res.set(headers);
+
 		library.db.all("SELECT id FROM blocks WHERE height > (SELECT height FROM blocks where id=$id) LIMIT 1440", {$id: req.query.id}, function (err, blocks) {
 			if (err) {
 				console.log(err);
@@ -132,12 +142,14 @@ Transport.prototype.onBlockchainReady = function () {
 	});
 
 	router.get("/blocks/milestone", function (req, res) {
+		res.set(headers);
+
 		// get milestone block
 		var lastBlockId = req.query.lastBlockId,
 			lastMilestoneBlockId = req.query.lastMilestoneBlockId;
 
 		if (lastBlockId == modules.blocks.getLastBlock().id) {
-			return res.status(200).json({ last: true, milestoneBlockIds: [lastBlockId] });
+			return res.status(200).json({last: true, milestoneBlockIds: [lastBlockId]});
 		}
 
 		var blockId, height, jump, limit;
@@ -172,14 +184,14 @@ Transport.prototype.onBlockchainReady = function () {
 			}
 		], function (errors) {
 			if (errors) {
-				return res.status(200).json({ error: errors.pop() });
+				return res.status(200).json({error: errors.pop()});
 			} else {
-				library.db.get("SELECT id FROM blocks WHERE height = $height", { $height: height }, function (err, block) {
+				library.db.get("SELECT id FROM blocks WHERE height = $height", {$height: height}, function (err, block) {
 					if (err) {
 						console.log(err);
-						return res.status(200).json({ error: "Internal sql error" });
+						return res.status(200).json({error: "Internal sql error"});
 					} else if (!block) {
-						return res.status(200).json({ error: "Internal error" });
+						return res.status(200).json({error: "Internal error"});
 					} else {
 						blockId = block.id;
 
@@ -189,7 +201,7 @@ Transport.prototype.onBlockchainReady = function () {
 							},
 							function (next) {
 								milestoneBlockIds.push(blockId);
-								library.db.get("SELECT id FROM blocks WHERE height = $height", { $height: height }, function (err, block) {
+								library.db.get("SELECT id FROM blocks WHERE height = $height", {$height: height}, function (err, block) {
 									if (err) {
 										return next(err);
 									} else if (!block) {
@@ -203,9 +215,9 @@ Transport.prototype.onBlockchainReady = function () {
 							},
 							function (err) {
 								if (err) {
-									return res.status(200).json({ error: err });
+									return res.status(200).json({error: err});
 								} else {
-									return res.status(200).json({ milestoneBlockIds: milestoneBlockIds });
+									return res.status(200).json({milestoneBlockIds: milestoneBlockIds});
 								}
 							}
 						)
@@ -216,6 +228,7 @@ Transport.prototype.onBlockchainReady = function () {
 	});
 
 	router.get("/blocks", function (req, res) {
+		res.set(headers);
 		// get 1400+ blocks with all data (joins) from provided block id
 		modules.blocks.loadBlocksPart(1440, 0, req.query.lastBlockId, function (err, res) {
 			return res.status(200).json({blocks: !err ? res : []});
@@ -223,8 +236,9 @@ Transport.prototype.onBlockchainReady = function () {
 	});
 
 	router.get("/transactions", function (req, res) {
+		res.set(headers);
 		// need to process headers from peer
-		return res.status(200).json({ transactions: modules.transactions.getUnconfirmedTransactions() });
+		return res.status(200).json({transactions: modules.transactions.getUnconfirmedTransactions()});
 	});
 
 	router.get('/weight', function (req, res) {
