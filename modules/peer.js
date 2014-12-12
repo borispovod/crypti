3aa11fe1594d17fb7de82592904aed50e359dfad
landgraf.paul <landgraf.paul@gmail.com>
2014-12-12 01:58:48 +0100

    add headers, update basic auth, update peers
    Former-commit-id: eea183e19f07ca3b7f52ffd3a117a28ec8c3d1a2 [formerly b1a8009a2927a3ac483e430563bf469242bd91ac]
    Former-commit-id: e5fd29c7d9b399ec55a60ce07f028c890fefb720

--- modules/peer.js
+++ modules/peer.js
@@ -17,22 +17,46 @@ Peer.prototype.run = function (scope) {
 	modules = scope;
 }
 
-
 Peer.prototype.list = function (limit, cb) {
 	limit = limit || 100;
 	var params = {$limit: limit};
 
-	library.db.all("select ip, port from peers where blocked = 0 ORDER BY RANDOM() LIMIT $limit", params, cb)
+	library.db.all("select ip, port, state, os, sharePort, version from peers where state != 0 and sharePort = 1 ORDER BY RANDOM() LIMIT $limit", params, cb);
 }
 
-Peer.prototype.updateHeaders = function (ip, headers, cb) {
-	var st = library.db.prepare("UPDATE peers SET port = $port WHERE ip = $ip AND (port != $port)");
-	st.bind({
-		$ip : ip,
-		$port : headers.port
-	});
+Peer.prototype.state = function (ip, port, state, cb) {
+	var st = library.db.prepare("UPDATE peers SET state = $state WHERE ip = $ip and port = $port;");
+	st.bind({$state: state, $ip: ip, $port: port});
 	st.run(function (err) {
-		return cb(err);
+		err && console.log(err);
+		cb && cb()
+	});
+}
+
+Peer.prototype.update = function (peer, cb) {
+	library.db.serialize(function () {
+
+		var params = {
+			$ip: peer.ip,
+			$port: peer.port,
+			$state: peer.state,
+			$os: peer.os,
+			$sharePort: peer.sharePort,
+			$version: peer.version
+		}
+
+		var st = library.db.prepare("INSERT OR IGNORE INTO peers (ip, port, state, os, sharePort, version) VALUES ($ip, $port, $state, $os, $sharePort, $version);");
+		st.bind(params);
+		st.run();
+
+		var st = library.db.prepare("UPDATE peers SET state = $state, os = $os, sharePort = $sharePort, version = $version WHERE ip = $ip and port = $port;");
+		st.bind(params);
+		st.run();
+
+		st.finalize(function (err) {
+			err && console.log(err);
+			cb && cb()
+		});
 	});
 }
 
@@ -42,50 +66,6 @@ Peer.prototype.count = function (cb) {
 	library.db.get("select count(rowid) as count from peers", params, function (err, res) {
 		cb(err, res.count)
 	})
-}
-
-Peer.prototype.ban = function (list, cb) {
-	list = util.isArray(list) ? list : [list];
-
-	var stmt = library.db.prepare("update peers set blocked = 1 where ip = $ip");
-
-	for (var i = 0, length = list.length; i < length; i++) {
-		stmt.run({
-			$ip: list[i].ip
-		});
-	}
-
-	stmt.finalize(cb);
-}
-
-Peer.prototype.add = function (list, cb) {
-	list = util.isArray(list) ? list : [list];
-
-	var stmt = library.db.prepare("insert or ignore into peers (ip, port, blocked) values ($ip, $port, 0)");
-
-	for (var i = 0, length = list.length; i < length; i++) {
-		stmt.run({
-			$ip: list[i].ip,
-			$port: list[i].port
-		});
-	}
-
-	stmt.finalize(cb);
-}
-
-Peer.prototype.remove = function (list, cb) {
-	list = util.isArray(list) ? list : [list];
-
-	var stmt = library.db.prepare("delete from peers where ip = $ip and port = $port");
-
-	for (var i = 0, length = list.length; i < length; i++) {
-		stmt.run({
-			$ip: list[i].ip,
-			$port: list[i].port
-		});
-	}
-
-	stmt.finalize(cb);
 }
 
 //export
