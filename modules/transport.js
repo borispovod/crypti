5ab3a0d6341937163e9771d4797154ba30af0dfe
Boris Povod <boris@crypti.me>
2014-12-16 23:38:21 +0300

    fixed load blocks for peers
    Former-commit-id: f0856ade0d44e65ae6f560c7f3d01fe57ba358fd

--- modules/transport.js
+++ modules/transport.js
@@ -230,7 +230,7 @@ Transport.prototype.onBlockchainReady = function () {
 	router.get("/blocks", function (req, res) {
 		res.set(headers);
 		// get 1400+ blocks with all data (joins) from provided block id
-		modules.blocks.loadBlocksPart(1440, 0, req.query.lastBlockId, function (err, blocks) {
+		modules.blocks.loadBlocksPart(1440, 0, req.query.lastBlockId, false, function (err, blocks) {
 			return res.status(200).json({blocks: !err ? blocks : []});
 		});
 	});
