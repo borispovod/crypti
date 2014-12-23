93bfc9db10549d82c806c7e95646c4b3498a7525
landgraf.paul <landgraf.paul@gmail.com>
2014-12-23 22:15:55 +0100

    draft delegate

---
+++ modules/delegates.js
@@ -0,0 +1,91 @@
+var crypto = require('crypto'),
+	bignum = require('bignum'),
+	ed = require('ed25519'),
+	params = require('../helpers/params.js');
+
+var Router = require('../helpers/router.js');
+
+//private
+var modules, library, self;
+var delegates;
+
+//public
+function Delegates() {
+	self = this;
+	library = scope;
+	delegates = {};
+
+	var router = new Router();
+
+	router.use(function (req, res, next) {
+		if (modules) return next();
+		res.status(500).send({success: false, error: 'loading'});
+	});
+
+	router.put('/', function (req, res) {
+		var secret = params.string(req.body.secret),
+			publicKey = params.buffer(req.body.publicKey, 'hex'),
+			secondSecret = params.string(req.body.secondSecret);
+
+		var hash = crypto.createHash('sha256').update(secret, 'utf8').digest();
+		var keypair = ed.MakeKeypair(hash);
+
+		if (publicKey) {
+			if (keypair.publicKey.toString('hex') != publicKey.toString('hex')) {
+				return res.json({success: false, error: "Please, provide valid secret key of your account"});
+			}
+		}
+
+		var account = modules.accounts.getAccountByPublicKey(keypair.publicKey);
+
+		if (!account) {
+			return res.json({success: false, error: "Account doesn't has balance"});
+		}
+
+		if (!account.publicKey) {
+			return res.json({success: false, error: "Open account to make transaction"});
+		}
+
+		var transaction = {
+			type: 4,
+			subtype: 0,
+			amount: 0,
+			recipientId: null,
+			senderPublicKey: account.publicKey,
+			timestamp: timeHelper.getNow()
+		};
+
+		modules.transactions.sign(secret, transaction);
+
+		if (account.secondSignature) {
+			if (!secondSecret || secondSecret.length == 0) {
+				return res.json({success: false, error: "Provide second secret key"});
+			}
+
+			modules.transactions.secondSign(secondSecret, transaction);
+		}
+
+		modules.transactions.processUnconfirmedTransaction(transaction, true, function (err) {
+			if (err) {
+				return res.json({success: false, error: err});
+			}
+
+			res.json({success: true, transaction: transaction});
+		});
+	})
+
+	library.app.use('/api/delegates', router);
+	library.app.use(function (err, req, res, next) {
+		if (!err) return next();
+		library.logger.error('/api/delegates', err)
+		res.status(500).send({success: false, error: err});
+	});
+
+	setImmediate(cb, null, self);
+}
+
+Delegates.prototype.run = function (scope) {
+	modules = scope;
+}
+
+module.exports = Delegates;
