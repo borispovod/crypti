6471036293d5a003fdfedb49b6578270068cf3e9
landgraf.paul <landgraf.paul@gmail.com>
2014-12-29 18:17:15 +0100

    params update

--- helpers/params.js
+++ helpers/params.js
@@ -2,26 +2,31 @@ var util = require('util');
 
 module.exports = {
 	int: function (val) {
-		val = parseInt(val, 10);
-		return isNaN(val) ? 0 : val;
+		var res = parseInt(val, 10);
+		return isNaN(res) ? 0 : res;
 	},
 	string: function (val) {
 		return (val || '').toString();
 	},
 	bool: function (val) {
-		val = ((val + '').toLowerCase() == "false" || (val + '').toLowerCase() == "f") ? false : val;
-		return !!val;
+		var res = ((val + '').toLowerCase() == "false" || (val + '').toLowerCase() == "f") ? false : val;
+		return !!res;
 	},
 	float: function (val) {
-		val = parseFloat(val);
-		return isNaN(val) ? 0 : val;
+		var res = parseFloat(val);
+		return isNaN(res) ? 0 : res;
 	},
 	buffer: function (val, mode) {
-		return new Buffer(val || '', mode);
+		try {
+			var res = new Buffer(val || '', mode)
+		} catch (e) {
+			var res = new Buffer('');
+		}
+		return res;
 	},
 	object: function (val) {
-		val = Object.prototype.toString.call(val) == "[object Object]" ? val : {};
-		return val;
+		var res = Object.prototype.toString.call(val) == "[object Object]" ? val : {};
+		return res;
 	},
 
 	array: function (val) {
@@ -29,7 +34,7 @@ module.exports = {
 	},
 
 	variant: function (val) {
-		val = val === undefined ? '' : val;
-		return val;
+		var res = val === undefined ? '' : val;
+		return res;
 	}
 }
