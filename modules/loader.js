9dd3bb2e4b4ce7f5e3e26b53b7c6c7e6cd577044
landgraf.paul <landgraf.paul@gmail.com>
2015-01-07 19:53:14 +0100

    merge fix

--- modules/loader.js
+++ modules/loader.js
@@ -86,7 +86,7 @@ Loader.prototype.run = function (scope) {
 					});
 				})
 			}, function (err) {
-				modules.dbLite.close();
+				library.dbLite.close();
 				if (err) {
 					library.logger.error('loadBlocksOffset', err);
 					if (err.block) {
