bf47a3dee793e0b31e4ac3be50cc8339a9362cd4
rumkin <dev@rumk.in>
2015-01-14 19:55:36 +0000

    Sandbox cpu limit tests added

--- test/sandbox/index.js
+++ test/sandbox/index.js
@@ -21,6 +21,7 @@ describe('Sandbox.', function(){
 
     it('Instance should be an object', function(){
         sandbox = new Sandbox({
+            cpuLimit : 25,
             stdio : [null, 1, 2]
         });
         should(sandbox).type('object', 'Its\' instance is an object');
@@ -45,9 +46,34 @@ describe('Sandbox.', function(){
 
         it('Sandbox context should has Domain and it should to capture errors.', function(done){
             sandbox.run(readFile('source-domain.js'), function(err){
-                should(err).not.equal(null, '`err` is not null');
+                should(err).not.equal(null, '`err` not empty');
                 should(err).be.instanceof(Object, '`err` instance of Object');
                 should(err).hasOwnProperty('message').instanceof(String, '`err.message` is a String');
+                done();
+            });
+        });
+
+        it('Should reach sandbox.timeLimit', function(done){
+            sandbox.timeLimit = 100;
+            sandbox.run('setTimeout(done, 1000)', function(err){
+                should(err).not.equal(null, '`err` not empty');
+                sandbox.timeLimit = Infinity;
+                done();
+            });
+        });
+
+        it('Should not reach sandbox.cpuLimit 25%', function(done){
+            sandbox.run('setTimeout(done, 1000)', function(err){
+                should(err).equal(null, '`err` is empty');
+                done();
+            });
+        });
+
+        it('Should reach sandbox.cpuLimit 1%', function(done){
+            sandbox.cpuLimit = 1;
+            sandbox.run('setTimeout(done, 1000)', function(err){
+                should(err).not.equal(null, '`err` is not null');
+                sandbox.cpuLimit = 25;
                 done();
             });
         });
