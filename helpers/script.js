193a2d007ffac9b13a705c869d500860a759cfe7
Boris Povod <boris@crypti.me>
2015-01-31 08:10:18 +0300

    fix

--- helpers/script.js
+++ helpers/script.js
@@ -6,7 +6,9 @@ exports.getBytes = getBytes;
 function getBytes(script) {
 	var inputBuffer = new Buffer(script.parameters, 'hex');
 	var codeBuffer = new Buffer(script.code, 'hex');
-	var objBuffer = Buffer.concat([inputBuffer, codeBuffer])
+	var name = new Buffer(script, "utf8");
+	var description = new Buffer(script.description? description : "", "utf8");
+	var objBuffer = Buffer.concat([inputBuffer, codeBuffer, name, description]);
 
 	return objBuffer;
 }
