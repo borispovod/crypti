6ab6f17b3d98f4837ad99f4df6d008aa6f5b8493
landgraf.paul <landgraf.paul@gmail.com>
2015-01-31 15:51:53 +0100

    fix params.hex
    Former-commit-id: 462bee18af5800391ca0ab68dc1179e97660dc87

--- helpers/params.js
+++ helpers/params.js
@@ -2,34 +2,34 @@ var util = require('util');
 
 module.exports = {
 	int: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		var res = parseInt(val, 10);
 		return isNaN(res) ? 0 : res;
 	},
 	string: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		return (val || '').toString();
 	},
 	bool: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		var res = ((val + '').toLowerCase() == "false" || (val + '').toLowerCase() == "f") ? false : val;
 		return !!res;
 	},
 	float: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		var res = parseFloat(val);
 		return isNaN(res) ? 0 : res;
 	},
 	buffer: function (val, mode, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		try {
@@ -39,18 +39,20 @@ module.exports = {
 		}
 		return res;
 	},
-	hex: function(val, nullable){
-		if (nullable === true && (val === null || val === undefined)){
+	hex: function (val, nullable) {
+		var res = this.string(val, nullable);
+		if (nullable === true && res === null) {
 			return null;
 		}
-		if (/^[a-f0-9].(..)$/i.exec(val)){
-			return val;
-		}else{
-			return null;
+		if (res && res.length && (res.length % 2 == 0) && (/^[a-f0-9]+$/i.exec(res))) {
+			return res;
+		} else {
+			console.log('not valid hex', res)
+			return null; //not valid hex
 		}
 	},
 	object: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		var res = Object.prototype.toString.call(val) == "[object Object]" ? val : {};
@@ -58,14 +60,14 @@ module.exports = {
 	},
 
 	array: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		return util.isArray(val) ? val : [];
 	},
 
 	variant: function (val, nullable) {
-		if (nullable === true && (val === null || val === undefined)){
+		if (nullable === true && (val === null || val === undefined)) {
 			return null;
 		}
 		var res = val === undefined ? '' : val;
