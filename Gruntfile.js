4e512dd203af83629062e1c124a1f124d35f23cf
Boris Povod <boris@crypti.me>
2015-02-25 02:58:03 +0300

    package build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,15 @@ module.exports = function (grunt) {
 		'app.js'
 	];
 
+	var recipients = [
+		{
+			email: 'boris@crypti.me',
+			name: 'Boris Povod'
+		}
+	];
+
+	var config = require("./config.json");
+
 	grunt.initConfig({
 		obfuscator: {
 			files: files,
@@ -38,24 +47,91 @@ module.exports = function (grunt) {
 			}
 		},
 
+		exec: {
+			dir : "mkdir  -p  ./builded/" + config.version,
+			public_dir: "mkdir  -p  ./builded/" + config.version + "/public",
+			app: "cp ./builded/app.js ./builded/" + config.version,
+			config: "cp ./config.json ./builded/" + config.version + "/config.json",
+			package: "cp ./package.json ./builded/" + config.version + "/package.json",
+			public_html: "cp ./public/{forging.html,loading.html,wallet.html} ./builded/" + config.version + "/public/",
+			public_assets: "cp -rf ./public/{images,partials,static} ./builded/" + config.version + "/public/"
+		},
+
 		compress: {
 			main: {
 				options: {
-					archive: 'crypti.zip'
+					archive: config.version + '.zip'
 				},
 				files: [
-					{src: ['builded/**'], dest: '/'}
+					{src: ['builded/' + config.version + '/**'], dest: './'}
 				]
+			}
+		},
+
+		gcloud: {
+			project: {
+				options: {
+					projectId: 'crypti-cloud',
+					bucket: 'crypti-testing',
+					keyFilename: '.gcloud.json'
+				},
+				files: [
+					{
+						src: config.version + ".zip",
+						dest: 'nodes'
+					}
+				]
+			}
+		},
+
+		nodemailer: {
+			options: {
+				transport: {
+					type: 'SMTP',
+					options: {
+						service: 'Gmail',
+						auth: {
+							user: 'helpdesk@crypti.me',
+							pass: 'U6XzQPM45MLJyk8'
+						}
+					}
+				},
+				recipients: recipients
+			},
+			message: {
+				options: {
+					from: "Crypti Versions <helpdesk@crypti.me>",
+					subject: 'Version ' + config.version + ' available now',
+					text: 'New version is avaliable now: http://storage.googleapis.com/crypti-testing/nodes/' + config.version + '.zip',
+					html: 'New version is avaliable now: http://storage.googleapis.com/crypti-testing/nodes/' + config.version + '.zip'
+				}
+			}
+		},
+		slack: {
+			options: {
+				endpoint: 'https://crypti.slack.com/services/hooks/slackbot?token=NfyJ9C4luNAn6k4sdzXHNplY',
+				channel: '#testing',
+				username: 'Crypti',
+				icon_url: 'http://vermilion1.github.io/presentations/grunt/images/grunt-logo.png' // if icon_emoji not specified
+			},
+			your_raget: {
+				text: '@sebastian @boris @landgraf_paul @stas New version (' + config.version + ') of Crypti available: http://storage.googleapis.com/crypti-testing/nodes/' + config.version + '.zip'
 			}
 		}
 	});
 
+	//http://storage.googleapis.com/crypti-testing/nodes/0.2.0.zip
+
 	grunt.loadNpmTasks('grunt-obfuscator');
-	grunt.loadNpmTasks("grunt-jscrambler");
-	//grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-jscrambler');
+	grunt.loadNpmTasks('grunt-exec');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-gcloud');
+	grunt.loadNpmTasks('grunt-nodemailer');
+	grunt.loadNpmTasks('grunt-slack-hook');
 
 	grunt.registerTask("default", ["obfuscator"]);
-
-	//compress removed
 	grunt.registerTask("release", ["default", "jscrambler"]);
+	//"jscrambler",
+	grunt.registerTask("package", ["default",  "exec", "compress", "gcloud:project", "nodemailer:message"]);
 };
