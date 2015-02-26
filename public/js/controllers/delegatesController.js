e5299544a8058fd2cd4c1cc90f21faffa49476db
vnekrasova <vnekrasova@local.ru>
2015-02-26 01:11:18 +0100

    delegates with ng-table & cached reload

--- public/js/controllers/delegatesController.js
+++ public/js/controllers/delegatesController.js
@@ -1,27 +1,56 @@
 require('angular');
 
-angular.module('webApp').controller('delegatesController', ['$scope', '$rootScope', '$http', "userService", "$interval", "$filter", "ngTableParams", function ($rootScope, $scope, $http, userService, $interval, $filter, ngTableParams) {
-    $scope.address = userService.address;
-    var data = [];
-    $scope.getDelegates = function () {
-        $http.get("/api/delegates/", {params: {orderBy: "vote:desc", limit: 10, offset:0}})
-            .then(function (resp) {
-                var data = resp.data.delegates;
-                $scope.tableTopDelegates = new ngTableParams({
-                    page: 1,            // show first page
-                    count: 101           // count per page
+angular.module('webApp').controller('delegatesController', ['$scope', '$rootScope', '$http', "userService", "$interval", "$filter", "ngTableParams", "delegateService",
+    function ($rootScope, $scope, $http, userService, $interval, $filter, ngTableParams, delegateService) {
 
-                }, {
-                    counts: [], // hide page counts control
-                    total: data.length, // length of data
+        $scope.address = userService.address;
 
-                    getData: function ($defer, params) {
+        $scope.voteList = {
+            list: [],
+            inList: function(publicKey){
+                return this.list.indexOf(publicKey)!=-1;
+            },
+            vote: function(publicKey){
+                if (this.inList(publicKey)){
+                    this.list.splice(this.list.indexOf(publicKey), 1);
+                }
+                else {
+                    this.list.push(publicKey);
+                }
+            }
+        };
 
-                        $defer.resolve(data);
-                    }
-                });
-            });
-    }();
+        $scope.vote = function (publicKey) {
 
+        };
 
-}]);
+        $scope.balance = userService._unconfirmedBalance;
+
+        //Top deletates
+        $scope.tableTopDelegates = new ngTableParams({
+            page: 1,            // show first page
+            count: 101,
+            sorting: {
+                rate: 'asc'     // initial sorting
+            }
+        }, {
+            counts: [], // hide page counts control
+            getData: function ($defer, params) {
+                delegateService.getTopList($defer, params, $scope.filter);
+            }
+        });
+
+        $scope.tableTopDelegates.settings().$scope = $scope;
+
+        $scope.$watch("filter.$", function () {
+            $scope.tableTopDelegates.reload();
+        });
+
+        $scope.updateTop = $interval(function () {
+            if (new Date() - delegateService.cachedTOP.time >= 1000 * 10) {
+                $scope.tableTopDelegates.reload();
+            }
+        }, 1000 * 1);
+        //end Top delegates
+
+    }]);
--- public/js/services/delegateService.js
+++ public/js/services/delegateService.js
@@ -1,3 +1,64 @@
-/**
- * Created by Vera on 19.02.2015.
- */
+require('angular');
+
+angular.module('webApp').service('delegateService', function ($http, $filter) {
+
+    function filterData(data, filter) {
+        return $filter('filter')(data, filter)
+    }
+
+    function orderData(data, params) {
+        return params.sorting() ? $filter('orderBy')(data, params.orderBy()) : filteredData;
+    }
+
+    function sliceData(data, params) {
+        return data.slice((params.page() - 1) * params.count(), params.page() * params.count())
+    }
+
+    function transformData(data, filter, params) {
+        return sliceData(orderData(filterData(data, filter), params), params);
+    }
+
+    var delegates = {
+        cachedTOP: {data: [], time: new Date()},
+        getTopList: function ($defer, params, filter) {
+            if (delegates.cachedTOP.data.length > 0 && new Date() - delegates.cachedTOP.time < 1000 * 10) {
+                var filteredData = filterData(delegates.cachedTOP.data, filter);
+                var transformedData = sliceData(orderData(filteredData, params), params);
+                params.total(filteredData.length)
+                $defer.resolve(transformedData);
+            }
+            else {
+                $http.get("/api/delegates/", {params: {orderBy: "rate:asc", limit: 101, offset: 0}})
+                    .then(function (response) {
+                        angular.copy(response.data.delegates, delegates.cachedTOP.data);
+                        delegates.cachedTOP.time = new Date();
+                        params.total(response.data.delegates.length);
+                        var filteredData = $filter('filter')(response.data.delegates, filter);
+                        var transformedData = transformData(response.data.delegates, filter, params)
+                        $defer.resolve(transformedData);
+                    });
+            }
+        },
+        getStandbyList: function ($defer, params, filter) {
+            if (service.cachedData.length > 0) {
+                console.log("using cached data")
+                var filteredData = filterData(service.cachedData, filter);
+                var transformedData = sliceData(orderData(filteredData, params), params);
+                params.total(filteredData.length)
+                $defer.resolve(transformedData);
+            }
+            else {
+                console.log("fetching data")
+                $http.get("http://www.json-generator.com/api/json/get/bUAZFEHxCG").success(function (resp) {
+                    angular.copy(resp, service.cachedData)
+                    params.total(resp.length)
+                    var filteredData = $filter('filter')(resp, filter);
+                    var transformedData = transformData(resp, filter, params)
+                    $defer.resolve(transformedData);
+                });
+            }
+
+        }
+    };
+    return delegates;
+});
