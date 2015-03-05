cfeff3b970fef5a22518c6ecd77c716a367c6908
vnekrasova <vnekrasova@local.ru>
2015-03-05 22:58:59 +0100

    delegate registration
    Former-commit-id: 3fa6d9d5bed411d08628f55b34bd55c4c862a629

--- public/js/controllers/forgingController.js
+++ public/js/controllers/forgingController.js
@@ -1,82 +1,102 @@
 require('angular');
 
-angular.module('webApp').controller('forgingController', ['$scope', '$rootScope', '$http', "userService", "$interval", "companyModal", "forgingModal", function ($rootScope, $scope, $http, userService, $interval, companyModal, forgingModal) {
-	$scope.address = userService.address;
-	$scope.effectiveBalance = userService.effectiveBalance;
-	$scope.totalBalance = userService.balance;
-	$scope.unconfirmedBalance = userService.unconfirmedBalance;
-	$scope.loadingBlocks = true;
+angular.module('webApp').controller('forgingController', ['$scope', '$rootScope', '$http', "userService", "$interval", "companyModal", "forgingModal", "registrationDelegateModal", "delegateService",
+    function ($rootScope, $scope, $http, userService, $interval, companyModal, forgingModal, registrationDelegateModal, delegateService) {
+        $scope.address = userService.address;
+        $scope.effectiveBalance = userService.effectiveBalance;
+        $scope.totalBalance = userService.balance;
+        $scope.unconfirmedBalance = userService.unconfirmedBalance;
+        $scope.loadingBlocks = true;
 
-	$scope.getBlocks = function () {
-		$http.get("/api/blocks", {
-			params: {
-				generatorPublicKey: userService.publicKey,
-				limit: 20,
-				orderBy: "height:desc"
-			}
-		})
-			.then(function (resp) {
-				$scope.blocks = resp.data.blocks;
-				$scope.loadingBlocks = false;
-			});
-	}
+        $scope.getBlocks = function () {
+            $http.get("/api/blocks", {
+                params: {
+                    generatorPublicKey: userService.publicKey,
+                    limit: 20,
+                    orderBy: "height:desc"
+                }
+            })
+                .then(function (resp) {
+                    $scope.blocks = resp.data.blocks;
+                    $scope.loadingBlocks = false;
+                });
+        }
 
-	$scope.getForgedAmount = function () {
-		$http.get("/api/delegates/forging/getForgedByAccount", {params: {generatorPublicKey: userService.publicKey}})
-			.then(function (resp) {
-				$scope.totalForged = resp.data.fees;
-			});
-	}
+        $scope.getForgedAmount = function () {
+            $http.get("/api/delegates/forging/getForgedByAccount", {params: {generatorPublicKey: userService.publicKey}})
+                .then(function (resp) {
+                    $scope.totalForged = resp.data.fees;
+                });
+        }
 
-	$scope.getForging = function () {
-		$http.get("/api/delegates/forging/status", {params: {publicKey: userService.publicKey}})
-			.then(function (resp) {
-				$scope.forging = resp.data.enabled;
-				userService.setForging($scope.forging);
-			});
-	}
+        $scope.getDelegate = function () {
+            delegateService.getDelegate(userService.senderPublicKey, function (response) {
+                $scope.delegate = response;
+                userService.setDelegate($scope.delegate);
+            });
+        }
 
-	$scope.infoInterval = $interval(function () {
-		$scope.getBlocks();
-		$scope.getForgedAmount();
-		$scope.getForging();
-	}, 1000 * 30);
+        $scope.getForging = function () {
+            $http.get("/api/delegates/forging/status", {params: {publicKey: userService.publicKey}})
+                .then(function (resp) {
+                    $scope.forging = resp.data.enabled;
+                    userService.setForging($scope.forging);
+                });
+        }
+
+        $scope.infoInterval = $interval(function () {
+            $scope.getBlocks();
+            $scope.getForgedAmount();
+            $scope.getDelegate();
+            $scope.getForging();
+        }, 1000 * 30);
 
 
-	$scope.getBlocks();
-	$scope.getForgedAmount();
-	$scope.getForging();
+        $scope.getBlocks();
+        $scope.getForgedAmount();
+        $scope.getDelegate();
+        $scope.getForging();
 
 
-	$scope.enableForging = function () {
-		$scope.forgingModal = forgingModal.activate({
-			forging: false,
-			totalBalance: userService.unconfirmedBalance,
-			destroy: function () {
-				$scope.forging = userService.forging;
-				$scope.getForging();
-			}
-		})
-	}
+        $scope.enableForging = function () {
+            $scope.forgingModal = forgingModal.activate({
+                forging: false,
+                totalBalance: userService.unconfirmedBalance,
+                destroy: function () {
+                    $scope.forging = userService.forging;
+                    $scope.getForging();
+                }
+            })
+        }
 
-	$scope.disableForging = function () {
-		$scope.forgingModal = forgingModal.activate({
-			forging: true,
-			totalBalance: userService.unconfirmedBalance,
-			destroy: function () {
-				$scope.forging = userService.forging;
-				$scope.getForging();
-			}
-		})
-	}
+        $scope.disableForging = function () {
+            $scope.forgingModal = forgingModal.activate({
+                forging: true,
+                totalBalance: userService.unconfirmedBalance,
+                destroy: function () {
+                    $scope.forging = userService.forging;
+                    $scope.getForging();
+                }
+            })
+        }
 
-	$scope.newCompany = function () {
-		$scope.companyModal = companyModal.activate({
-			totalBalance: $scope.unconfirmedBalance,
-			destroy: function () {
-				$scope.getInfo();
-			}
-		});
-	}
+        $scope.registrationDelegate = function () {
+            $scope.registrationDelegateModal = registrationDelegateModal.activate({
+                totalBalance: userService.unconfirmedBalance,
+                destroy: function () {
+                    $scope.delegate = userService.delegate;
+                    $scope.getDelegate();
+                }
+            })
+        }
 
-}]);
+        $scope.newCompany = function () {
+            $scope.companyModal = companyModal.activate({
+                totalBalance: $scope.unconfirmedBalance,
+                destroy: function () {
+                    $scope.getInfo();
+                }
+            });
+        }
+
+    }]);
