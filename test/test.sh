d534e2218942d8af02c2038a21d51876c1df2b4f
landgraf.paul <landgraf.paul@gmail.com>
2015-04-24 00:46:05 +0200

    clean

--- test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "NODE" ]; then
-    NODE=node
-fi
-
-$NODE node_modules/.bin/mocha -t 2000 test/test.js || exit 1;
-$NODE node_modules/.bin/mocha -t 0 test/ui/login.js || exit 1;
