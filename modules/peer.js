b719d4bdd2a4dcaa2e2c4268989461f2a60da036
Boris Povod <boris@crypti.me>
2015-05-29 12:23:09 +0300

    fixed build file in build version

--- modules/peer.js
+++ modules/peer.js
@@ -65,7 +65,7 @@ function attachApi() {
 	});
 
 	router.get('/version', function (req, res) {
-		fs.readFile(path.join(__dirname, '..', 'build'), 'utf8', function (err, data) {
+		fs.readFile(path.join(__dirname, 'build'), 'utf8', function (err, data) {
 			if (err) {
 				library.logger.error("Can't read build file: " + err);
 				return res.json({success: false, error: "Can't read 'build' file, see logs"});
