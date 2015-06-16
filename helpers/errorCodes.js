318fd5b831b3301dcf78ef940203f4a6e8d0d22f
Boris Povod <boris@crypti.me>
2015-06-16 18:56:24 +0300

    updated error messages

--- helpers/errorCodes.js
+++ helpers/errorCodes.js
@@ -3,15 +3,15 @@ var util = require('util');
 var errorCodes = {
 	VOTES: {
 		INCORRECT_RECIPIENT: {
-			message: "Incorrect recipient %s, in vote transaction recipient is same that sender",
+			message: "Invalid recipient ID: %s. Recipient ID identical to sender ID",
 			args: ['recipientId']
 		},
 		MINIMUM_DELEGATES_VOTE: {
-			message: "Empty votes: %s",
+			message: "To vote, you must select at least one delegate: TX ID: %s",
 			args: ["id"]
 		},
 		MAXIMUM_DELEGATES_VOTE: {
-			message: "You can only vote for a maximum of 33 delegates at any one time: %s",
+			message: "You can only vote for a maximum of 33 delegates at any one time: TX ID: %s",
 			args: ["id"]
 		},
 		ALREADY_VOTED_UNCONFIRMED: {
@@ -19,113 +19,113 @@ var errorCodes = {
 			args: ["id"]
 		},
 		ALREADY_VOTED_CONFIRMED: {
-			message: "Can't verify votes, you already voted for this delegate: %s",
+			message: "You already voted for this delegate: TX ID: %s",
 			args: ["id"]
 		}
 	},
 	USERNAMES: {
 		INCORRECT_RECIPIENT: {
-			message: "Incorrect recipient",
+			message: "Invalid recipient. Please try again",
 			args: []
 		},
 		INVALID_AMOUNT: {
-			message: "Invalid amount of transaction: %s",
+			message: "Invalid amount. Please try again. TX ID: %s",
 			args: ["id"]
 		},
 		EMPTY_ASSET: {
-			message: "Empty transaction asset for username transaction: %s",
+			message: "Username TX: Empty transaction asset. Please try again. TX ID: %s",
 			args: ["id"]
 		},
 		ALLOW_CHARS: {
-			message: "Username can only contain alphanumeric characters with the exception of !@$&_.: %s",
+			message: "Username can only contain alphanumeric characters with the exception of !@$&_. TX ID: %s",
 			args: ["id"]
 		},
 		USERNAME_LIKE_ADDRESS: {
-			message: "Username can't be like an address: %s",
+			message: "Username cannot be a potential address. TX ID: %s",
 			args: ["id"]
 		},
 		INCORRECT_USERNAME_LENGTH: {
-			message: "Incorrect username length: %s",
+			message: "Invalid username length. Please use 1 to 16 alphanumeric characters. Username: %s",
 			args: ["asset.username.alias"]
 		},
 		EXISTS_USERNAME: {
-			message: "The username you entered is already in use. Please try a different name.: %s",
+			message: "The username you entered is already in use. Please try a different name. TX ID: %s",
 			args: ["id"]
 		},
 		ALREADY_HAVE_USERNAME: {
-			message: "The account already has username",
+			message: "This account already has a username registered. You can only have 1 username per account.",
 			args: ["id"]
 		}
 	},
 	ACCOUNTS: {
 		ACCOUNT_PUBLIC_KEY_NOT_FOUND: {
-			message: "Account public key can't be found: %s",
+			message: "Unable to find account public key for this address: %s",
 			args: ["address"]
 		},
 		ACCOUNT_DOESNT_FOUND: {
-			message: "Account doesn't found: %s",
+			message: "Account not found. Address: %s",
 			args: ["address"]
 		},
 		INVALID_ADDRESS: {
-			message: "Invalid address: %s",
+			message: "%s is an invalid address. Please provide a valid Crypti address",
 			args: ["address"]
 		}
 	},
 	DELEGATES: {
 		INVALID_RECIPIENT: {
-			message: "Invalid recipientId: %s",
+			message: "Invalid recipient ID. Please try again. TX ID: %s",
 			args: ["id"]
 		},
 		INVALID_AMOUNT: {
-			message: "Invalid amount: %i",
+			message: "Invalid amount: %i. Please try again",
 			args: ["amount"]
 		},
 		EMPTY_TRANSACTION_ASSET: {
-			message: "Empty transaction asset for delegate transaction: %s",
+			message: "Delegate TX: Empty transaction asset. TX ID: %s",
 			args: ["id"]
 		},
 		USERNAME_CHARS: {
-			message: "Username can only contain alphanumeric characters with the exception of !@$&_.: %s",
+			message: "Delegate names can only contain alphanumeric characters with the exception of !@$&_. Name: %s",
 			args: ["asset.delegate.username"]
 		},
 		USERNAME_LIKE_ADDRESS: {
-			message: "Username can't be like an address: %s",
+			message: "Delegate names cannot be a potential address. Name: %s",
 			args: ["asset.delegate.username"]
 		},
 		USERNAME_IS_TOO_SHORT: {
-			message: "Delegate name is too short: %s",
+			message: "Delegate name is too short. Please use 1 to 20 characters. Name: %s",
 			args: ["asset.delegate.username"]
 		},
 		USERNAME_IS_TOO_LONG: {
-			message: "Delegate name is longer then 20 chars: ",
+			message: "Delegate name is longer then 20 characters. Please use 1 to 20 characters. Name: %s",
 			args: ["asset.delegate.username"]
 		},
 		EXISTS_USERNAME: {
-			message: "The delegate name you entered is already in use. Please try a different name.: %s",
+			message: "The delegate name you entered is already in use. Please try a different name. Name: %s",
 			args: ["asset.delegate.username"]
 		},
 		EXISTS_DELEGATE: {
-			message: "Your account are delegate already",
+			message: "This account is already a delegate",
 			args: []
 		},
 		DELEGATE_NOT_FOUND: {
-			message: "Delegate not found",
+			message: "Unable to find delegate",
 			args: []
 		},
 		FORGER_PUBLIC_KEY: {
-			message: "Provide generatorPublicKey in request",
+			message: "Please provide generatorPublicKey in request",
 			args: []
 		},
 		FORGING_ALREADY_ENABLED: {
-			message: "Forging on this account already enabled",
+			message: "Forging is already enabled on this account",
 			args: []
 		},
 		DELEGATE_NOT_FOUND: {
-			message: "Delegate for this secret not found",
+			message: "Delegate not found. Please check the password",
 			args: []
 		},
 		FORGER_NOT_FOUND: {
-			message: "Forger with this public key not found",
+			message: "The public key you provided does not belong to any delegate",
 			args: []
 		},
 		WRONG_USERNAME: {
@@ -135,21 +135,21 @@ var errorCodes = {
 	},
 	PEERS: {
 		PEER_NOT_FOUND: {
-			message: "Peers not found",
+			message: "Unable to find peer",
 			args: []
 		},
 		LIMIT: {
-			message: "Max limit is %i",
+			message: "Maximum number of peers is: %i",
 			args: ['limit']
 		},
 		INVALID_PEER: {
-			message: "Engine is starting",
+			message: "Peers: Engine is starting",
 			args: []
 		}
 	},
 	COMMON: {
 		LOADIND: {
-			message: "Engine is starting",
+			message: "Please wait: Engine is starting",
 			args: []
 		},
 		DB_ERR: {
@@ -157,61 +157,65 @@ var errorCodes = {
 			args: []
 		},
 		INVALID_API: {
-			message: "Api not found",
+			message: "API request was not found. Please check your request and try again",
 			args: []
 		},
 		INVALID_SECRET_KEY: {
-			message: "Please, provide valid secret key of your account",
+			message: "Invalid password. Please try again",
 			args: []
 		},
 		OPEN_ACCOUNT: {
-			message: "Open your account to make transaction",
+			message: "In order to send a transaction, you are need to first open (log in) your account on this node",
 			args: []
 		},
 		SECOND_SECRET_KEY: {
-			message: "Provide second secret key",
+			message: "Please provide secondary account password",
 			args: []
 		},
 		ACCESS_DENIED: {
-			message: "Access denied",
+			message: "Access denied. Please ensure you are allowed access: config.json file, whitelist sections",
 			args: []
 		}
 	},
 	BLOCKS: {
 		BLOCK_NOT_FOUND: {
-			message: "Block not found",
+			message: "Unable to find block",
 			args: []
 		},
 		WRONG_ID_SEQUENCE: {
-			message: "Invalid ids sequence",
+			message: "Invalid ID sequence",
 			args: []
 		}
 	},
 	TRANSACTIONS: {
 		INVALID_RECIPIENT: {
-			message: "Invalid recipientId: %s",
+			message: "Invalid recipient ID: %s. Please try again",
 			args: ["recipientId"]
 		},
 		INVALID_AMOUNT: {
-			message: "Invalid transaction amount: %i",
+			message: "Invalid amount. You cannot send %i XCR. Please try again",
 			args: ["amount"]
 		},
 		TRANSACTION_NOT_FOUND: {
-			message: "Transaction not found",
+			message: "Unable to find transaction",
 			args: []
 		},
 		TRANSACTIONS_NOT_FOUND: {
-			message: "Transactions not found",
+			message: "Unable to find transactions",
 			args: []
 		},
 		RECIPIENT_NOT_FOUND: {
-			message: "Recipient is not found",
+			message: "Unable to find recipient",
 			args: []
+		},
+		INVALID_ADDRESS: {
+			message: "%s is an invalid address. Please provide a valid Crypti address",
+			args: ["address"]
 		}
 	},
 	SIGNATURES: {
 		INVALID_ASSET: {
-			message: "Empty transaction asset for signature transaction: %s",
+			message: "SIGNATURES: Empty transaction asset. TX ID: %s",
 			args: ["id"]
 		},
 		INVALID_AMOUNT: {
@@ -219,21 +223,21 @@ var errorCodes = {
 			args: ["amount"]
 		},
 		INVALID_LENGTH: {
-			message: "Invalid length for signature public key: %s",
+			message: "Invalid length for signature public key. TX ID: %s",
 			args: ["id"]
 		},
 		INVALID_HEX: {
-			message: "Invalid hex in signature public key: %s",
+			message: "Invalid hex found in signature public key. TX ID: %s",
 			args: ["id"]
 		}
 	},
 	CONTACTS: {
 		USERNAME_DOESNT_FOUND: {
-			message: "Account doesn't found: %s",
+			message: "Unable to find this account: %s",
 			args: []
 		},
 		SELF_FRIENDING: {
-			message: "Can´t add yourself in contacts",
+			message: "Error: You cannot add yourself as your own contact",
 			args: []
 		}
 	}
