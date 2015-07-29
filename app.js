1249e9029c6062f9cf056442e618d7754296584e
Boris Povod <boris@crypti.me>
2015-07-29 19:51:49 +0300

    fixed username

--- app.js
+++ app.js
@@ -239,7 +239,7 @@ d.run(function () {
 			scope.network.app.use(bodyParser.json());
 			scope.network.app.use(methodOverride());
 
-			var ignore = ['id', 'blockId', 'transactionId', 'address', 'recipientId', 'senderId', 'senderUsername', 'recipientUsername', 'previousBlock'];
+			var ignore = ['id', 'blockId', 'transactionId', 'username', 'address', 'recipientId', 'senderId', 'senderUsername', 'recipientUsername', 'previousBlock'];
 			scope.network.app.use(queryParser({
 				parser: function(value, radix, name) {
 					if (ignore.indexOf(name) >= 0) {
