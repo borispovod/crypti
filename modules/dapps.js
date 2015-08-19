a5ec21abaa46d144d74a5ba548b00b7e6180adf2
Pavel <landgraf.paul@gmail.com>
2015-08-19 13:23:56 +0200

    fix dapp

--- modules/dapps.js
+++ modules/dapps.js
@@ -84,7 +84,11 @@ function Transfer() {
 	}
 
 	this.apply = function (trs, sender, cb) {
-		self.message(trs.asset.dapptransfer.dappid, {recipientId: sender.address, amount: trs.amount, transactionId: trs.id}, cb);
+		self.message(trs.asset.dapptransfer.dappid, {
+			recipientId: sender.address,
+			amount: trs.amount,
+			transactionId: trs.id
+		}, cb);
 	}
 
 	this.undo = function (trs, sender, cb) {
@@ -795,7 +799,7 @@ private.attachApi = function () {
 							library.logger.error(err);
 							return res.json({success: false, error: "Sql error, check logs"})
 						} else {
-							library.dbLite.query("SELECT rowid FROM dapps_search WHERE dapps_search MATCH $q", {q: query.q + "*"}, function (err, rows) {
+							library.dbLite.query("SELECT rowid FROM dapps_search WHERE dapps_search MATCH $q", {q: query.q + "*"}, ["rowid"], function (err, rows) {
 								if (err) {
 									library.logger.error(err);
 									return res.json({success: false, error: "Sql error, check logs"});
@@ -1480,11 +1484,11 @@ private.dappRoutes = function (dapp, cb) {
 						}, function (err, body) {
 							if (!err && body.error) {
 								err = body.error;
+							} else if (err) {
+								body = {error: err.toString()}
 							}
 
-							body = ((err || typeof body != "object") ? {error: err} : body);
-							var resultBody = extend(body, {success: !err});
-							res.json(resultBody);
+							res.json({success: !err, result: body});
 						});
 					});
 				}
@@ -1613,13 +1617,16 @@ private.launchApp = function (dApp, params, cb) {
 			port: peer.port,
 			dappid: dApp.transactionId
 		}, cb);
-	}, function () {
+	}, function (err) {
+		if (err) {
+			return setImmediate(cb, err);
+		}
 		modules.sql.createTables(dApp.transactionId, dappConfig.db, function (err) {
 			if (err) {
-				return setImmediate(err);
+				return setImmediate(cb, err);
 			}
 
-			var sandbox = new Sandbox(path.join(dappPath, "index.js"), dApp.transactionId, params, private.apiHandler, true);
+			var sandbox = new Sandbox(path.join(dappPath, "index.js"), dApp.transactionId, params, private.apiHandler, false);
 			private.sandboxes[dApp.transactionId] = sandbox;
 
 			sandbox.on("exit", function () {
@@ -1832,7 +1839,9 @@ DApps.prototype.onNewBlock = function (block, broadcast) {
 				message: {id: block.id, height: block.height}
 			}
 		}, function (err, body) {
-			library.logger.error("dapp", err)
+			if (err) {
+				library.logger.error("onNewBlock message", err)
+			}
 		});
 	})
 }
@@ -1866,6 +1875,11 @@ shared.getCommonBlock = function (req, cb) {
 		"inner join dapptransfers dt on dt.transactionId = t.id and dt.dappid = $dappid", {
 		dappid: req.dappid,
 		type: TransactionTypes.DAPPTRANSFER
+	}, {
+		height: Number,
+		id: String,
+		senderId: String,
+		amount: String
 	}, function (err, rows) {
 		if (err) {
 			return cb("Sql error");
